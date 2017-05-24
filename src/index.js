f793779123eb738c9d8184517eaa4d2ef78684c0
Jon Ambas <jon@jonambas.com>
2017-05-24 12:21:50 -0400

    Update index.js and index.html to latest CRA version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 import './index.scss';
 
@@ -8,3 +9,4 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+registerServiceWorker();
