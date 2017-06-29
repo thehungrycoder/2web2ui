009a24b54d479cc45471df1208d2dd323db92966
Jon Ambas <jon@jonambas.com>
2017-06-29 14:52:33 -0400

    Update nav with icons

--- src/components/Navigation/Item.js
+++ src/components/Navigation/Item.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
 
+import { Icon } from '@sparkpost/matchbox';
 import styles from './Navigation.module.scss';
-
-const Chevron = () => (
-  <svg className={styles.chevron} height="1792" viewBox="0 0 1792 1792" width="1792" xmlns="http://www.w3.org/2000/svg">
-  <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"/></svg>
-);
 
 export class Item extends Component {
   constructor (props) {
@@ -64,16 +60,16 @@ export class Item extends Component {
         { children
           ? <li>
               <a onClick={() => this.handleParentClick()} className={linkClasses}>
-                <span className={styles.icon}>{ icon }</span>
+                <span className={styles.iconWrapper}><Icon name={icon} size={18} className={styles.icon} /></span>
                 { label }
-                <Chevron />
+                <Icon name='ChevronLeft' className={styles.chevron} />
               </a>
               { this.renderChildren() }
             </li>
 
           : <li>
               <Link to={ to } className={linkClasses}>
-                { icon && <span className={styles.icon}>{ icon }</span>}
+                { icon && <span className={styles.iconWrapper}><Icon name={icon} size={18} className={styles.icon} /></span> }
                 { label }
               </Link>
             </li>
