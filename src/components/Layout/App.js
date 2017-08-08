11e525aca9e8d84352f094116e7c6cd9c36442c7
Jon Ambas <jon@jonambas.com>
2017-08-08 15:36:48 -0400

    FAD-5252 Add GlobalError

--- src/components/Layout/App.js
+++ src/components/Layout/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
-import styles from './Layout.module.scss';
 import Navigation from '../Navigation/Navigation';
+import GlobalError from '../GlobalError/GlobalError';
+import styles from './Layout.module.scss';
 
-class App extends Component {
-  render () {
-    const { children } = this.props;
-
-    return (
-      <div className={`${styles.wrapper} ${styles.accent}`}>
-        <div className={styles.aside}>
-          <Navigation />
-        </div>
-        <main role="main" className={styles.content}>
-          <div className={styles.container}>
-            { children }
-          </div>
-        </main>
+const App = ({ children }) => (
+  <div className={`${styles.wrapper} ${styles.accent}`}>
+    <div className={styles.aside}>
+      <Navigation />
+    </div>
+    <main role="main" className={styles.content}>
+      <div className={styles.container}>
+        { children }
       </div>
-    );
-  }
-}
+      <div className={styles.appError}>
+        <GlobalError />
+      </div>
+    </main>
+  </div>
+);
 
-const mapStateToProps = ({ apiFailure }) => ({ apiFailure });
-export default connect(mapStateToProps)(App);
+export default App;
--- src/components/Layout/Form.js
+++ src/components/Layout/Form.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
+import GlobalError from '../GlobalError/GlobalError';
 import styles from './Layout.module.scss';
 
-class Form extends Component {
-  render () {
-    const { children } = this.props;
-    return (
-      <div className={styles.wrapper}>
-        <main role="main" className={styles.formContent}>
-          <div className={styles.formContainer}>
-            { children }
-          </div>
-        </main>
+const Form = ({ children }) => (
+  <div className={styles.wrapper}>
+    <main role="main" className={styles.formContent}>
+      <div className={styles.formContainer}>
+        { children }
       </div>
-    );
-  }
-}
+      <div className={styles.formError}>
+        <GlobalError />
+      </div>
+    </main>
+  </div>
+);
 
 export default Form;
