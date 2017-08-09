d6d627b9d35eacc450996479566f1f81238aec33
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-08-09 12:48:57 -0400

    Added consolidated-tst config for now

--- src/config/env/consolidated-tst.js
+++ src/config/env/consolidated-tst.js
@@ -1,5 +1,5 @@
 export default {
-  apiBase: 'http://api.sparkpost.dev/api/v1',
+  apiBase: 'https://api-uat.tst.sparkpost.com/api/v1',
   authentication: {
     headers: {
       Authorization: 'Basic bXN5c1dlYlVJOmZhODZkNzJlLTYyODctNDUxMy1hZTdmLWVjOGM4ZmEwZDc2Ng==',
