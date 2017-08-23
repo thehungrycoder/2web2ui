11b55fff668e64764e269fa166e49418756a178e
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-08-23 12:00:49 -0400

    Fixing module resolution

--- config/paths.js
+++ config/paths.js
@@ -50,7 +50,7 @@ module.exports = {
   appSrc: resolveApp('src'),
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('src/setupTests.js'),
-  appNodeModules: resolveApp('node_modules'),
+  appNodeModules: resolveApp('src'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
 };
