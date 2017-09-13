5a15cf668c890df27cc481f7780aa4360e9511a0
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-09-13 15:00:24 -0400

    OS X SERIOUSLY GAWD

--- src/pages/Webhooks/index.js
+++
@@ -1,9 +0,0 @@
-import ListPage from './ListPage';
-import CreatePage from './CreatePage';
-import DetailsPage from './DetailsPage';
-
-export default {
-  ListPage,
-  CreatePage,
-  DetailsPage
-};
