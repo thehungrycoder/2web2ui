932af413dbe85a8204edd493f6e11e87d0d1ccb7
Brian Kemper <brian.kemper@sparkpost.com>
2017-10-05 14:14:35 -0400

    FAD-5464 Converted selectUsers to use reselect

--- src/selectors/users.js
+++ src/selectors/users.js
@@ -1,8 +1,15 @@
 import fp from 'lodash/fp';
+import { createSelector } from 'reselect';
+
+const getCurrentUser = (state) => state.currentUser;
+const getUsers = (state) => state.users.entities;
 
 // Get, reduce, enrich, and sort list of users
-export const selectUsers = ({ currentUser, users }) => fp.flow(
-  fp.values,
-  fp.map((user) => ({ ...user, isCurrentUser: currentUser.username === user.username })),
-  fp.sortBy(({ name }) => name.toLowerCase()) // downcase to match current UI
-)(users.entities);
+export const selectUsers = createSelector(
+  [getCurrentUser, getUsers],
+  (currentUser, users) => fp.flow(
+    fp.values,
+    fp.map((user) => ({ ...user, isCurrentUser: currentUser.username === user.username })),
+    fp.sortBy(({ name }) => name.toLowerCase()) // downcase to match current UI
+  )(users)
+);
