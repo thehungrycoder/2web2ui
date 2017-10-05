c282acee24b50cf11240df9a7fb45d6aa7a6fc40
Brian Kemper <brian.kemper@sparkpost.com>
2017-10-05 14:53:46 -0400

    FAD-5464 Refactored users reducer tests to use jest-in-case

--- src/reducers/tests/users.test.js
+++ src/reducers/tests/users.test.js
@@ -1,22 +1,40 @@
+import cases from 'jest-in-case';
 import usersReducer, { initialState } from '../users';
 
-it('returns state without deleted user', () => {
-  const action = {
+const state = {
+  ...initialState,
+  entities: {
+    'test-user-one': {
+      access: 'admin',
+      username: 'test-user-one'
+    },
+    'test-user-two': {
+      access: 'reporting',
+      username: 'test-user-two'
+    }
+  }
+};
+
+const TEST_CASES = {
+  'removes user from entities': {
     type: 'DELETE_USER_SUCCESS',
+    meta: { data: { username: 'test-user-two' } }
+  },
+  'ignores update for unknown user': {
+    type: 'UPDATE_USER_SUCCESS',
+    meta: { data: { username: 'unknown-user' } }
+  },
+  'updates users access level from admin to reporting': {
+    type: 'UPDATE_USER_SUCCESS',
     meta: {
       data: {
-        username: 'test-user',
+        access_level: 'reporting',
+        username: 'test-user-one'
       }
     }
   }
-  const state = {
-    ...initialState,
-    entities: {
-      'test-user': {
-        username: 'test-user',
-      }
-    }
-  }
+};
 
-  expect(usersReducer(state, action)).not.toHaveProperty('entities.test-user');
-})
+cases('Users reducer', (action) => {
+  expect(usersReducer(state, action)).toMatchSnapshot();
+}, TEST_CASES);
