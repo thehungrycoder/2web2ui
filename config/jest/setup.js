57a41d3902ab8d69c6f06dd35677244ce06078dc
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-10-07 17:18:40 -0400

    Updated deps for react 16, lots of tests failed, leaving it here for now

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,3 +1,8 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 // Provides enzyme assertions.
 // See https://github.com/blainekasten/enzyme-matchers#assertions
 import 'jest-enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
