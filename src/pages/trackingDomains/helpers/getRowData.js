14608c864e7d7e5e4d2246f1ee0ebb1f99925af4
Jose Zamora <jgzamora@terpmail.umd.edu>
2017-10-10 08:50:41 -0400

    resolve status

--- src/pages/trackingDomains/helpers/getRowData.js
+++ src/pages/trackingDomains/helpers/getRowData.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const resolveStatus = ({ verified, compliance_status }) => {
+  if (verified) {
+    if (compliance_status === 'valid') {
+      return 'verified';
+    } else {
+      return compliance_status;
+    }
+  } else {
+    return 'unverified';
+  }
+};
+
 /*
  Tracking Domains getRowData passed to TableCollection in ListPage.
- TODO: this needs to be a Table.Row component to better handle hasSubaccounts
 */
 const getRowData = ({ domain, status, subaccount_id }) => ([
   <Link to={`/account/trackingDomains/${domain}`}>{domain}</Link>,
-  status.verified ? 'verified' : 'unverified',
+  resolveStatus(status),
   subaccount_id ? subaccount_id : null
 ]);
+
 
 export default getRowData;
