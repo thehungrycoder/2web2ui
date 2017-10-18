afdbb785b0b1ed8a15b93dc71cc17bbd5f99ff84
mhossain <mohammad.hossain@sparkpost.com>
2017-10-18 15:11:18 -0400

    FAD-5465 Use jest to create mock fn

--- src/pages/ipPools/tests/ListPage.test.js
+++ src/pages/ipPools/tests/ListPage.test.js
@@ -12,7 +12,7 @@ describe('IP Pools List Page', () => {
         {name: 'Test Pool 1', id: 101, ips: [{external_ip: 1111}, {external_ip: 2222}]},
         {name: 'Test Pool 2', id: 102, ips: []}
       ],
-      listPools: () => {}
+      listPools: jest.fn(() => [])
     };
     const wrapper = shallow(<IpPoolsList {...props} />);
     expect(wrapper).toMatchSnapshot();
@@ -21,7 +21,7 @@ describe('IP Pools List Page', () => {
   it('should show alert upon error', () => {
     const props = {
       ipPools: [],
-      listPools: () => {},
+      listPools: jest.fn(() => []),
       error: {
         message: 'Uh oh! It broke.' // renders as details
       },
