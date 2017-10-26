be5a03e532e225729d24b4c4d9e13e04f9b321c6
Jose Zamora <jgzamora@terpmail.umd.edu>
2017-10-26 13:13:06 -0400

    initial loadable

--- src/pages/templates/components/Editor.js
+++ src/pages/templates/components/Editor.js
@@ -1,2 +1,20 @@
-import asyncComponent from 'src/helpers/asyncComponent';
-export default asyncComponent(() => import('./_Editor'));
+import React, { Component } from 'react';
+import loadable from 'react-loadable';
+
+const LoadableEditor = loadable({
+  loader: () => import('./_Editor'),
+  render(loaded, props) {
+    console.log(props); //eslint-disable-line
+    const Component = loaded.default;
+    return <Component {...props}/>;
+  },
+  loading() { //Actual Loading Component goes here
+    return <div>Loading...</div>;
+  }
+});
+
+export default class Editor extends Component {
+  render() {
+    return <LoadableEditor {...this.props}/>;
+  }
+}
