7ec08547963959034bdf3f515c1ec20b49981d58
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-10-28 15:24:25 -0400

    FAD-5494 adds tests for create tracking domain

--- src/pages/trackingDomains/CreatePage.js
+++ src/pages/trackingDomains/CreatePage.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Page } from '@sparkpost/matchbox';
-import CreateForm from './components/Form';
+import CreateForm from './components/CreateForm';
 import { createTrackingDomain } from 'src/actions/trackingDomains';
 import { showAlert } from 'src/actions/globalAlert';
 
 export class CreatePage extends Component {
 
   onSubmit = (data) => {
-    const { history, showAlert } = this.props;
-    this.props.createTrackingDomain(data).then(() => {
+    const { history, showAlert, createTrackingDomain } = this.props;
+    return createTrackingDomain(data).then(() => {
       showAlert({ type: 'success', message: `Successfully added ${data.domain}` });
       history.push('/account/tracking-domains');
     });
