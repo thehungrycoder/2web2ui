5abcd78cb954878b0eab9f5188842d5b550af14a
mhossain <mohammad.hossain@sparkpost.com>
2017-10-30 15:17:14 -0400

    add/update selector

--- src/selectors/templates.js
+++ src/selectors/templates.js
@@ -1,2 +1,3 @@
 export const getTemplates = (state) => state.templates.list;
-export const getTemplateById = (state, props) => state.templates.byId[props.match.params.id] || { draft: {}, published: {}};
+export const getTemplateById = (state, id) => state.templates.byId[id] || { draft: {}, published: {}};
+export const cloneTemplate = (template) => Object.assign({ ...template }, { name: `${template.name} Copy`, id: `${template.id}-copy` });
