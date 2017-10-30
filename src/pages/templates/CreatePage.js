0ce9df697a12eb65a1495d33f4b048b3cd4ef281
mhossain <mohammad.hossain@sparkpost.com>
2017-10-30 15:18:26 -0400

    render cloned template in create form

--- src/pages/templates/CreatePage.js
+++ src/pages/templates/CreatePage.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { reduxForm, formValueSelector } from 'redux-form';
+import _ from 'lodash';
+
 // Actions
-import { create } from '../../actions/templates';
+import { create, getDraft } from 'src/actions/templates';
+import { showAlert } from 'src/actions/globalAlert';
+
+// Selectors
+import { getTemplateById, cloneTemplate } from 'src/selectors/templates';
 
 // Components
 import Form from './components/Form';
@@ -13,18 +19,48 @@ import { Page, Grid } from '@sparkpost/matchbox';
 const FORM_NAME = 'templateCreate';
 
 class CreatePage extends Component {
-  state = {
-    shouldRedirect: false
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shouldRedirect: false,
+      isDuplicating: !!props.match.params.cloneFrom
+    };
   }
 
+  componentDidMount() {
+    const { match, getDraft } = this.props;
+
+    getDraft(match.params.cloneFrom).catch((err) => err);
+  }
+
+  // componentDidUpdate() {
+  //   const { loading, template, showAlert } = this.props;
+
+  //   if (loading || !template) {
+  //     return;
+  //   }
+
+  //   const { draft = {}} = template;
+
+  //   if (!Object.keys(draft).length) {
+  //     this.setState({ redirectTo: '/templates/' }); // Redirect if no draft or published found
+  //     showAlert({ type: 'error', message: 'Could not find template' });
+  //   }
+  // }
+
   handleCreate(values) {
-    const { create } = this.props;
+    const { create, showAlert } = this.props;
     return create(values)
-      .then(() => this.setState({ shouldRedirect: true }));
+      .then(() => this.setState({ shouldRedirect: true }))
+      .catch((err) => {
+        const details = err.response.data.errors[0].description || err.message;
+        return showAlert({ type: 'error', message: 'Could not create template', details: details });
+      });
   }
 
   render() {
-    const { id, handleSubmit, submitting } = this.props;
+    const { id, handleSubmit, submitting, match } = this.props;
 
     if (this.state.shouldRedirect) {
       return <Redirect to={`/templates/edit/${id}`} />;
@@ -46,7 +82,8 @@ class CreatePage extends Component {
       <Page
         primaryAction={primaryAction}
         breadcrumbAction={backAction}
-        title='New Template' >
+        title= { match.params.cloneFrom ? 'Duplicate Template' : 'New Template' }>
+
         <Grid>
           <Grid.Column xs={12} lg={4}>
             <Form newTemplate={true} name={FORM_NAME} />
@@ -61,12 +98,28 @@ class CreatePage extends Component {
 }
 
 const selector = formValueSelector(FORM_NAME);
-const mapStateToProps = (state) => ({
-  id: selector(state, 'id')
-});
+const mapStateToProps = (state, props) => {
+  const id = selector(state, 'id');
+  const template = getTemplateById(state, props.match.params.cloneFrom);
 
-const formOptions = {
-  form: FORM_NAME
+console.log(template); //eslint-disable-line
+  if (_.get(props, 'match.params.cloneFrom') && !_.isEmpty(template.draft)) {
+    const drfatTemplate = cloneTemplate(template.draft);
+    return {
+      id: id,
+      loading: state.templates.getLoading,
+      initialValues: drfatTemplate
+    };
+  } else {
+    return {
+      id: id
+    };
+  }
 };
 
-export default connect(mapStateToProps, { create })(reduxForm(formOptions)(CreatePage));
+const formOptions = {
+  form: FORM_NAME,
+  enableReinitialize: true // required to update initial values from redux state
+};
+
+export default connect(mapStateToProps, { create, getDraft, showAlert })(reduxForm(formOptions)(CreatePage));
