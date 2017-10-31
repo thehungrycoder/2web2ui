307d166e402e2f8c1795b3f6e48b63d9a5e851d9
Jon Ambas <jon@jonambas.com>
2017-10-31 12:09:39 -0400

    FAD-5589 Add bounce route

---
+++ src/pages/reports/bounce/BouncePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BouncePage extends Component {
+  render() {
+    return (
+      <div>Bounce</div>
+    );
+  }
+}
+
+export default BouncePage;
--- src/pages/reports/index.js
+++ src/pages/reports/index.js
@@ -1,5 +1,7 @@
+import BouncePage from './bounce/BouncePage.js';
 import SummaryPage from './summary/SummaryPage.js';
 
 export default {
+  BouncePage,
   SummaryPage
 };
