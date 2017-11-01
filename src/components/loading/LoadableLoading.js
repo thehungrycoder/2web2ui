627e60bbf75365744ef6fa680d3ce0572c4b5fb1
Jose Zamora <jgzamora@terpmail.umd.edu>
2017-11-01 11:25:57 -0400

    fixing loadable typo

--- src/components/loading/LoadableLoading.js
+++ src/components/loading/LoadableLoading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Loading } from 'src/components/loading/Loading';
 
-const LoadbleLoading = ({ pastDelay }) => {
+const LoadableLoading = ({ pastDelay }) => {
   // if loading has taken more than 200 milliseconds
   if (pastDelay) {
     return <Loading />;
@@ -10,4 +10,4 @@ const LoadbleLoading = ({ pastDelay }) => {
   }
 };
 
-export default LoadbleLoading;
+export default LoadableLoading;
