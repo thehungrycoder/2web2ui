b00de47193871bc7425ab4a88c8a8bc3d2c888ef
mhossain <mohammad.hossain@sparkpost.com>
2017-11-09 23:10:38 -0500

    Update password

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import { Page, Panel, Table } from '@sparkpost/matchbox';
 
 import { updateUser } from 'src/actions/users';
+import { confirmPassword } from 'src/actions/auth';
 
 import NameForm from './components/NameForm';
 import PasswordForm from './components/PasswordForm';
+import { showAlert } from 'src/actions/globalAlert';
 
 export class ProfilePage extends Component {
   updateName = (values) => {
@@ -15,7 +17,17 @@ export class ProfilePage extends Component {
   }
 
   updatePassword = (values) => {
+    const { username } = this.props.currentUser;
+    const { history, showAlert } = this.props;
+
     console.log(values); //eslint-disable-line
+    return this.props.confirmPassword(username, values.current_password)
+      .then(() => this.props.updateUser(username, { password: values.new_password }))
+      .catch((e) => {
+        console.log(e); //eslint-disable-line
+        showAlert({ type: 'error', message: 'Unable to update password' });
+        history.push('/account/profile');
+      });
   }
 
   render() {
@@ -63,4 +75,4 @@ const mapStateToProps = ({ form, account, currentUser }) => ({
   currentUser
 });
 
-export default connect(mapStateToProps, { updateUser })(ProfilePage);
+export default connect(mapStateToProps, { updateUser, confirmPassword, showAlert })(ProfilePage);
