fd9b68594c71c49fef7375b8223e31ff07fed241
Bob Evans <robert.evans25@gmail.com>
2017-12-06 10:27:44 -0500

    FAD-5878 addressed feedback, removed redux from page not found

--- src/pages/pageNotFound/index.js
+++ src/pages/pageNotFound/index.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
+import React from 'react';
 import { EmptyState } from '@sparkpost/matchbox';
 
-export class PageNotFound extends Component {
-  render() {
-    return (<EmptyState
+export default function PageNotFound() {
+  return <EmptyState
       title='Page Not Found'
       image='Generic'>
          <p>We cannot find what you're looking for, use the navigation to find your way.</p>
-     </EmptyState>);
-  }
+     </EmptyState>;
 }
 
-export default connect(() => ({}), { })(PageNotFound);
--- src/pages/pageNotFound/tests/PageNotFound.snapshot.test.js
+++ src/pages/pageNotFound/tests/PageNotFound.snapshot.test.js
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import { PageNotFound } from '../index';
+import PageNotFound from '../index';
 
 describe('Page: Page Not Found tests', () => {
   let wrapper;
