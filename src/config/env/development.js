3f191210af94b4ae484eb3ac878777f272f4463d
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-12-14 15:04:45 -0500

    Updated config to use .test tld for local dev

--- src/config/env/development.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiBase: 'http://api.sparkpost.dev/api/v1',
-  authentication: {
-    headers: {
-      Authorization: 'Basic bXN5c1dlYlVJOmZhODZkNzJlLTYyODctNDUxMy1hZTdmLWVjOGM4ZmEwZDc2Ng==',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }
-};
