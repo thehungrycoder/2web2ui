23584e59ac82a7bef2be151ea538ab9fcba337b2
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-12-15 09:56:49 -0500

    FAD-5936 fixing lint errors in files so meta lulz

--- src/pages/users/CreatePage.js
+++ src/pages/users/CreatePage.js
@@ -38,7 +38,7 @@ const CreatePage = (props) => {
     return inviteUser(email, access).then((res) => {
       showAlert({
         type: 'success',
-        message: `We sent an invitation to ${email}! Once they sign up, you\'ll see them on this page`
+        message: `We sent an invitation to ${email}! Once they sign up, you'll see them on this page`
       });
       history.push('/account/users');
     })
