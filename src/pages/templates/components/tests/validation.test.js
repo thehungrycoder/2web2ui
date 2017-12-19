726b0743661b12fad1a6cd5a521cb9ebcbc3f67c
Jon Ambas <jon@jonambas.com>
2017-12-19 15:17:02 -0500

    FAD-5897 Add single substitution test

--- src/pages/templates/components/tests/validation.test.js
+++ src/pages/templates/components/tests/validation.test.js
@@ -32,6 +32,14 @@ describe('contentRequired', () => {
   });
 });
 
+describe('substitution', () => {
+  it('should handle substitution values', () => {
+    expect(validation.substitution('{{ sub_content }}')).toEqual(undefined);
+    expect(validation.substitution('{{ not valid }}')).toEqual('Substitution syntax error');
+    expect(validation.substitution('{{ not }} valid')).toEqual('Substitution syntax error');
+  });
+});
+
 describe('emailOrSubstitution', () => {
   it('should handle invalid email', () => {
     expect(validation.emailOrSubstitution('not an email')).toEqual('Invalid email or substitution value');
