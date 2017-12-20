680f373036e677bdf4ae3875092832a124abe856
mhossain <mohammad.hossain@sparkpost.com>
2017-12-20 16:34:59 -0500

    Add missing test for NameForm

--- src/pages/profile/components/tests/NameForm.test.js
+++ src/pages/profile/components/tests/NameForm.test.js
@@ -3,8 +3,25 @@ import React from 'react';
 import { NameForm } from '../NameForm';
 
 describe('Component: NameForm', () => {
+  let wrapper;
+  let props;
+  beforeEach(() => {
+    props = {
+      initialValues: {
+        firstName: 'foo',
+        lastName: 'bar'
+      }
+    };
+    wrapper = shallow(<NameForm {...props} />);
+  });
+
   it('should render correctly', () => {
-    expect(shallow(<NameForm />)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render correctly when updating', () => {
+    wrapper.setProps({ submitting: true });
+    expect(wrapper).toMatchSnapshot();
   });
 });
 
