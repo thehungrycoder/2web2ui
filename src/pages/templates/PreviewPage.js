9ea72deeb815c6acb7de6d380d39db411b7e0274
Brian Kemper <brian.kemper@sparkpost.com>
2017-12-20 09:58:14 -0500

    FAD-5342 Load template for preview

--- src/pages/templates/PreviewPage.js
+++ src/pages/templates/PreviewPage.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Page } from '@sparkpost/matchbox';
 
+import * as TemplateActions from 'src/actions/templates';
 import { Loading } from 'src/components';
 
+const IS_PUBLISHED_REGEX = new RegExp('/templates/preview/.+/published');
+
 export class PreviewPage extends Component {
+  componentDidMount() {
+    if (!this.props.template) {
+      this.getTemplate();
+    }
+  }
+
+  getTemplate() {
+    const { id } = this.props.match.params;
+
+    this.props.isPublished
+      ? this.props.getPublished(id)
+      : this.props.getDraft(id);
+  }
+
   render() {
     if (!this.props.template) {
       return <Loading />;
     }
 
+    const pageProps = {
+      breadcrumbAction: {
+        Component: Link,
+        content: 'Edit Template',
+        to: this.props.editTemplatePath
+      }
+    };
+
     return (
-      <Page>
+      <Page {...pageProps}>
       </Page>
     );
   }
 }
 
-const mapStateToProps = (state, props) => ({});
-const mapDispatchToProps = {};
+export const mapStateToProps = (state, props) => {
+  const isPublished = IS_PUBLISHED_REGEX.test(props.match.path);
+  const template = state.templates.byId[props.match.params.id] || {};
+  const type = isPublished ? 'published' : 'draft';
+
+  return {
+    editTemplatePath: isPublished
+      ? `/templates/edit/${props.match.params.id}/published`
+      : `/templates/edit/${props.match.params.id}`,
+    isPublished,
+    template: template[type]
+  };
+};
+
+const mapDispatchToProps = { ...TemplateActions };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PreviewPage));
--- src/pages/templates/test/PreviewPage.test.js
+++ src/pages/templates/test/PreviewPage.test.js
@@ -4,11 +4,60 @@ import { shallow } from 'enzyme';
 
 import { PreviewPage } from '../PreviewPage';
 
-const TEST_CASES = {
-  'renders loading': { template: undefined }
+const mockProps = {
+  editTemplatePath: '/path/to/edit/template',
+  getDraft: jest.fn(),
+  getPublished: jest.fn()
 };
 
+const TEST_CASES = {
+  'renders loading': {
+    match: { params: { id: 'test-template' }},
+    template: undefined
+  },
+  'renders draft template': {
+    match: { params: { id: 'test-draft' }},
+    template: {
+      id: 'test-draft'
+    }
+  },
+  'renders published template': {
+    match: { params: { id: 'test-published' }},
+    template: {
+      id: 'test-published'
+    }
+  }
+};
+
+afterEach(() => {
+  for (const key in mockProps) {
+    mockProps[key].mockClear && mockProps[key].mockClear();
+  }
+});
+
 cases('PreviewPage', (props) => {
-  const wrapper = shallow(<PreviewPage {...props} />);
+  const wrapper = shallow(<PreviewPage {...mockProps} {...props} />);
   expect(wrapper).toMatchSnapshot();
 }, TEST_CASES);
+
+test('load draft template', () => {
+  const props = {
+    isPublished: false,
+    match: { params: { id: 'test-draft' }},
+    template: undefined
+  };
+
+  shallow(<PreviewPage {...mockProps} {...props} />);
+  expect(mockProps.getDraft).toHaveBeenCalledWith('test-draft');
+});
+
+test('loads published template', () => {
+  const props = {
+    isPublished: true,
+    match: { params: { id: 'test-published' }},
+    template: undefined
+  };
+
+  shallow(<PreviewPage {...mockProps} {...props} />);
+  expect(mockProps.getPublished).toHaveBeenCalledWith('test-published');
+});
