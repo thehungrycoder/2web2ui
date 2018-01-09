9cc5d70fdcdddf34964d60be9cb227ba7293ae06
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2018-01-09 10:57:56 -0500

    FAD-5924 added missing describe for better test context

--- src/components/filterDropdown/tests/FilterDropdown.test.js
+++ src/components/filterDropdown/tests/FilterDropdown.test.js
@@ -1,44 +1,47 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-
 import { FilterDropdown } from '../FilterDropdown';
 
-const props = {
-  formName: 'myForm',
-  namespace: 'status',
-  displayValue: 'Status',
-  change: jest.fn(),
-  options: [
-    { content: 'Active', name: 'active' },
-    { content: 'Terminated', name: 'terminated' },
-    { content: 'Suspended', name: 'suspended' }
-  ]
-};
+describe('Component: FilterDropdown', () => {
 
-const e = {
-  stopPropagation: jest.fn()
-};
+  const props = {
+    formName: 'myForm',
+    namespace: 'status',
+    displayValue: 'Status',
+    change: jest.fn(),
+    options: [
+      { content: 'Active', name: 'active' },
+      { content: 'Terminated', name: 'terminated' },
+      { content: 'Suspended', name: 'suspended' }
+    ]
+  };
 
-let wrapper;
+  const e = {
+    stopPropagation: jest.fn()
+  };
 
-beforeEach(() => {
-  wrapper = shallow(<FilterDropdown {...props} />);
-});
+  let wrapper;
 
-it('renders correctly', () => {
-  expect(wrapper).toMatchSnapshot();
-});
+  beforeEach(() => {
+    wrapper = shallow(<FilterDropdown {...props} />);
+  });
 
-it('renders selected correctly', () => {
-  wrapper.setProps({ values: { suspended: true }});
-  expect(wrapper).toMatchSnapshot();
-});
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-it('handles click', () => {
-  wrapper.setProps({ values: { suspended: true }});
-  const changeSpy = jest.spyOn(wrapper.instance().props, 'change');
-  const eSpy = jest.spyOn(e, 'stopPropagation');
-  wrapper.instance().handleActionClick(e, 'terminated');
-  expect(changeSpy).toHaveBeenCalledWith(props.formName, `${props.namespace}.terminated`, true);
-  expect(eSpy).toHaveBeenCalled();
+  it('renders selected correctly', () => {
+    wrapper.setProps({ values: { suspended: true }});
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('handles click', () => {
+    wrapper.setProps({ values: { suspended: true }});
+    const changeSpy = jest.spyOn(wrapper.instance().props, 'change');
+    const eSpy = jest.spyOn(e, 'stopPropagation');
+    wrapper.instance().handleActionClick(e, 'terminated');
+    expect(changeSpy).toHaveBeenCalledWith(props.formName, `${props.namespace}.terminated`, true);
+    expect(eSpy).toHaveBeenCalled();
+  });
+
 });
