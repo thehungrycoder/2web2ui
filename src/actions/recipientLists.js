0d07e4c6eff3927f7bc397b0855527935ac23533
Ewan Dennis <ewan.dennis@sparkpost.com>
2018-01-11 12:59:26 +0000

    Update, delete actions+reducer

--- src/actions/recipientLists.js
+++ src/actions/recipientLists.js
@@ -19,15 +19,44 @@ export function listRecipientLists({ force } = {}) {
 }
 
 export function createRecipientList(data) {
-  return (dispatch) => dispatch(
-    sparkpostApiRequest({
-      type: 'CREATE_RECIPIENT_LIST',
-      meta: {
-        method: 'POST',
-        url: '/recipient-lists',
-        data
-      }
-    })
-  );
+  return sparkpostApiRequest({
+    type: 'CREATE_RECIPIENT_LIST',
+    meta: {
+      method: 'POST',
+      url: '/recipient-lists',
+      data
+    }
+  });
+}
+
+export function updateRecipientList(data, params) {
+  const { id, ...updateFields } = data;
+  return sparkpostApiRequest({
+    type: 'UPDATE_RECIPIENT_LIST',
+    meta: {
+      method: 'PUT',
+      url: `/recipient-lists/${id}`,
+      data: updateFields,
+      params
+    }
+  });
+}
+
+export function deleteRecipientList(id) {
+  return sparkpostApiRequest({
+    type: 'DELETE_RECIPIENT_LIST',
+    meta: {
+      method: 'DELETE',
+      url: `/recipient-lists/${id}`,
+      id
+    }
+  });
+}
+
+export function setCurrentRecipientList(id) {
+  return {
+    type: 'SET_CURRENT_RECIPIENT_LIST',
+    id
+  };
 }
 
--- src/reducers/recipientLists.js
+++ src/reducers/recipientLists.js
@@ -1,11 +1,12 @@
 const initialState = {
   list: [],
+  currentId: null,
   error: null,
   listLoaded: false,
   listLoading: false
 };
 
-export default (state = initialState, { type, payload, meta }) => {
+export default (state = initialState, { type, id, payload, meta }) => {
   switch (type) {
     case 'LIST_RECIPIENT_LISTS_PENDING':
       return { ...state, listLoading: true, error: null };
@@ -20,10 +21,35 @@ export default (state = initialState, { type, payload, meta }) => {
       return { ...state, loading: true };
 
     case 'CREATE_RECIPIENT_LIST_SUCCESS':
-      return { ...state, loading: false };
+      return {
+        ...state,
+        loading: false,
+        list: [ ...state.list, {
+          id: payload.id,
+          name: payload.name,
+          total_accepted_recipients: payload.total_accepted_recipients,
+          description: meta.data.description
+        }]
+      };
 
     case 'CREATE_RECIPIENT_LIST_FAIL':
       return { ...state, loading: false };
+
+    case 'SET_CURRENT_RECIPIENT_LIST':
+      return { ...state, currentId: id };
+
+    case 'UPDATE_RECIPIENT_LIST_SUCCESS':
+      return {
+        ...state,
+        list: state.list.map((item) =>
+          item.id === payload.id ? { ...item, ...payload } : item)
+      };
+
+    case 'DELETE_RECIPIENT_LIST_SUCCESS':
+      return {
+        ...state,
+        list: state.list.filter((item) => item.id !== meta.id)
+      };
 
     default:
       return state;
