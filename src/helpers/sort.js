cb970e795b070f847a5a25fcf0794c76c36e805a
mhossain <mohammad.hossain@sparkpost.com>
2018-01-17 22:42:13 -0500

    Remove unnecessary,evil helper func

--- src/helpers/sort.js
+++ src/helpers/sort.js
@@ -3,12 +3,3 @@ import _ from 'lodash';
 export function getSortedCollection(collection, sortColumn, sortDirection) {
   return _.orderBy(collection, sortColumn, sortDirection);
 }
-
-/**
- * Handles sorting. It sets states. So this function must be invoked with the context of the component (bind `this` to the component being used)
- * @param {*} column column name like recipient
- * @param {*} direction direction of sorting like asc/desc
- */
-export function handleSortChange(column, direction) {
-  this.setState({ sortColumn: column, sortDirection: direction }, this.forceUpdate);
-}
