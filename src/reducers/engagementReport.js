82ff000dc52cfbac677234f2764857359ca6238f
Brian Kemper <brian.kemper@sparkpost.com>
2018-01-23 10:14:57 -0500

    FAD-5978 Reducer cases to handle GET_ENGAGEMENT_TABLE_DATA

--- src/reducers/engagementReport.js
+++ src/reducers/engagementReport.js
@@ -3,6 +3,11 @@ const initialState = {
     data: {},
     error: null,
     loading: false
+  },
+  table: {
+    data: [],
+    error: null,
+    loading: false
   }
 };
 
@@ -14,6 +19,12 @@ export default (state = initialState, { type, payload }) => {
       return { ...state, chart: { data: state.chart.data, error: null, loading: true }};
     case 'GET_ENGAGEMENT_CHART_DATA_SUCCESS':
       return { ...state, chart: { data: payload[0], error: null, loading: false }};
+    case 'GET_ENGAGEMENT_TABLE_DATA_FAIL':
+      return { ...state, table: { data: [], error: payload, loading: false }};
+    case 'GET_ENGAGEMENT_TABLE_DATA_PENDING':
+      return { ...state, table: { data: state.table.data, error: null, loading: true }};
+    case 'GET_ENGAGEMENT_TABLE_DATA_SUCCESS':
+      return { ...state, table: { data: payload, error: null, loading: false }};
     default:
       return state;
   }
--- src/reducers/tests/engagementReport.test.js
+++ src/reducers/tests/engagementReport.test.js
@@ -1,20 +1,36 @@
 import engagementReportReducer from '../engagementReport';
 import cases from 'jest-in-case';
 
-cases('get engagement chart data', (action) => {
+cases('Engagement Report Reducer', (action) => {
   expect(engagementReportReducer(undefined, action)).toMatchSnapshot();
 }, {
-  'when failed': {
+  'when failed to get chart data': {
     type: 'GET_ENGAGEMENT_CHART_DATA_FAIL',
     payload: {
       message: 'Oh no!'
     }
   },
-  'when pending': {
+  'when pending to get chart data': {
     type: 'GET_ENGAGEMENT_CHART_DATA_PENDING'
   },
-  'when succeeds': {
+  'when succeeds to get chart data': {
     type: 'GET_ENGAGEMENT_CHART_DATA_SUCCESS',
     payload: [{ count_accepted: 237433, count_targeted: 272234 }]
+  },
+  'when failed to get table data': {
+    type: 'GET_ENGAGEMENT_TABLE_DATA_FAIL',
+    payload: {
+      message: 'Oh no!'
+    }
+  },
+  'when pending to get table data': {
+    type: 'GET_ENGAGEMENT_TABLE_DATA_PENDING'
+  },
+  'when succeeds to get table data': {
+    type: 'GET_ENGAGEMENT_TABLE_DATA_SUCCESS',
+    payload: [
+      { count_clicked: 1802, count_raw_clicked_approx: 1692, link_name: 'Raw URL' },
+      { count_clicked: 1402, count_raw_clicked_approx: 1020, link_name: 'Nintendo' }
+    ]
   }
 });
