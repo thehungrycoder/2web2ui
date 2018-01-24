813580e4071dccba0937c19e63b8e7292337f5b7
Jon Ambas <jon@jonambas.com>
2018-01-24 16:20:18 -0500

    FAD-5580 Add sending domain create page

---
+++ src/pages/sendingDomains/CreatePage.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import { create as createDomain } from 'src/actions/sendingDomains';
+import { showAlert } from 'src/actions/globalAlert';
+import { Page, Panel } from '@sparkpost/matchbox';
+
+import CreateForm from './components/CreateForm';
+
+export class CreatePage extends Component {
+  handleCreate = (values) => {
+    const { createDomain, history, showAlert } = this.props;
+
+    return createDomain(values)
+      .then(() => history.push(`/account/sending-domains/edit/${values.domain}`))
+      .catch((err) => {
+        showAlert({ type: 'error', message: 'Could not create template', details: err.message });
+      });
+  }
+
+  render() {
+    return (
+      <Page breadcrumbAction={{ content: 'Back to Sending Domains', Component: Link, to: '/account/sending-domains' }}>
+        <Panel title='Add a Domain'>
+          <CreateForm onSubmit={this.handleCreate} />
+        </Panel>
+      </Page>
+    );
+  }
+}
+
+export default withRouter(connect(null, { createDomain, showAlert })(CreatePage));
