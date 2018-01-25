4b1c5f4730ea12429e030e228cdbe453f47def3a
Bob Evans <robert.evans25@gmail.com>
2018-01-24 22:39:20 -0500

    FAD-5906 fixed issues after upgrading jest and rebasing

--- src/pages/webhooks/tests/ListPage.test.js
+++ src/pages/webhooks/tests/ListPage.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { renderRowData } from 'src/__testHelpers__';
+import { renderRowData } from 'src/__testHelpers__/renderHelpers';
 
 import { WebhooksList, getRowData } from '../ListPage';
 
