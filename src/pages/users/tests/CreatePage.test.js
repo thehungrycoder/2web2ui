4b2a3bd9fd4f6391fdaf2a9ce251ad5fe42346ce
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-01-24 23:51:24 -0500

    FAD-5933 added create page tests

--- src/pages/users/tests/CreatePage.test.js
+++ src/pages/users/tests/CreatePage.test.js
@@ -20,6 +20,7 @@ describe('Page: User Create Page', () => {
         push: jest.fn()
       }
     };
+    props.inviteUser.mockName('inviteUserMock');
     wrapper = shallow(<CreatePage {...props} />);
     instance = wrapper.instance();
     values = { email: 'email', access: 'access' };
@@ -52,8 +53,7 @@ describe('Page: User Create Page', () => {
 
   it('should show an alert on a 400 error', async() => {
     const err = { response: { status: 400 }};
-    props.inviteUser = jest.fn(() => Promise.reject(err));
-    props.inviteUser.mockName('inviteUserMock');
+    props.inviteUser.mockImplementation(() => Promise.reject(err));
 
     await instance.handleSubmit(values);
 
@@ -65,12 +65,36 @@ describe('Page: User Create Page', () => {
     expect(props.history.push).not.toHaveBeenCalled();
   });
 
-  it('should show an alert on a 409 error', () => {
+  it('should show an alert on a 409 error', async() => {
+    const err = { response: { status: 409, data: {
+      errors: [
+        { message: 'the real error message' }
+      ]
+    }}};
+    props.inviteUser.mockImplementation(() => Promise.reject(err));
 
+    await instance.handleSubmit(values);
+
+    expect(props.inviteUser).toHaveBeenCalledWith(values.email, values.access);
+    expect(props.showAlert).toHaveBeenCalledWith({
+      type: 'error',
+      message: 'the real error message'
+    });
+    expect(props.history.push).not.toHaveBeenCalled();
   });
 
-  it('should show an alert on a 500 error', () => {
+  it('should show an alert on a 500 error', async() => {
+    const err = { response: { status: 500 }};
+    props.inviteUser.mockImplementation(() => Promise.reject(err));
 
+    await instance.handleSubmit(values);
+
+    expect(props.inviteUser).toHaveBeenCalledWith(values.email, values.access);
+    expect(props.showAlert).toHaveBeenCalledWith({
+      type: 'error',
+      message: 'Unable to invite user.'
+    });
+    expect(props.history.push).not.toHaveBeenCalled();
   });
 
 });
