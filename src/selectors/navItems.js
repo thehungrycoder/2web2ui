62edee96c046cba30340a1c8cb0aa49e679f6899
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-01-25 15:28:07 -0500

    FAD-5933 fixed nasty navigation filtering reference bug

--- src/selectors/navItems.js
+++ src/selectors/navItems.js
@@ -4,42 +4,39 @@ import routes from 'src/config/routes';
 import navItems from 'src/config/navItems';
 import accessConditionState from './accessConditionState';
 
-const routesByPath = _.keyBy(routes, 'path');
-
-export const mapNavToRoutes = _.memoize((items) => items.map((item) => {
-  item.route = routesByPath[item.to];
-  if (item.children) {
-    item.children = mapNavToRoutes(item.children);
-  }
-  return item;
-}));
+export const mapNavToRoutes = _.memoize((items) => {
+  const routesByPath = _.keyBy(routes, 'path');
+  return items.map((item) => {
+    item.route = routesByPath[item.to];
+    if (item.children) {
+      item.children = mapNavToRoutes(item.children);
+    }
+    return item;
+  });
+});
 
 export function filterNavByAccess(items, accessConditionState) {
-  return items.filter((reference) => {
-    const item = { ...reference }; // prevent weird mutation bugs
-    const condition = _.get(item, 'route.condition');
+  const firstLevelFiltered = items.filter((item) => {
+    const condition = _.get(item, 'route.condition', () => true);
+    return condition(accessConditionState);
+  });
 
-    // if condition function returns false, block access here
-    if (typeof condition === 'function' && !condition(accessConditionState)) {
-      return false;
-    }
-
-    // do the filter process on children, if they exist
+  const childrenFiltered = firstLevelFiltered.map((item) => {
     if (item.children) {
       item.children = filterNavByAccess(item.children, accessConditionState);
-
-      // if all children are eliminated by access, remove this parent too
-      if (item.children.length === 0) {
-        return false;
-      }
     }
-
-    return true;
+    return item;
   });
+
+  return childrenFiltered.filter((item) => !item.children || item.children.length);
 }
 
 export function prepareNavItems(accessConditionState) {
-  return filterNavByAccess(mapNavToRoutes(navItems), accessConditionState);
+  const mapped = mapNavToRoutes(navItems);
+  return filterNavByAccess(mapped, accessConditionState);
 }
 
-export default createSelector([accessConditionState], prepareNavItems);
+export default createSelector(
+  [accessConditionState],
+  prepareNavItems
+);
