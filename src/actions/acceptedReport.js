d57e5f9f879249bbc72884c984c8a818ec247201
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-02-13 12:42:28 -0500

    FAD-6203 accepted report action tests

--- src/actions/acceptedReport.js
+++ src/actions/acceptedReport.js
@@ -5,12 +5,9 @@ import { ACCEPTED_METRICS } from 'src/constants';
 // Load new metrics for the accepted chart
 export function refreshAcceptedReport(updates = {}) {
   return (dispatch) => {
-    // TODO: consider having action creators that already pre-load their hard-coded metrics or as a selector
-    // TODO: This can probably become a selector for each query
     const params = getQueryFromOptions({ ...updates, metrics: ACCEPTED_METRICS });
 
     return Promise.all([
-      // TODO: Should this just be an action like getAcceptedAggregates with hard-coded metrics and type?
       dispatch(fetchDeliverability({ type: 'GET_ACCEPTED_AGGREGATES', params })),
       dispatch(fetchDeliveriesByAttempt(params))
     ]);
