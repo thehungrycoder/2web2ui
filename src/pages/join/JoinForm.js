68114a6f5e44dd177dccd2a15f1171c7d4bdd33e
mhossain <mohammad.hossain@sparkpost.com>
2018-02-14 07:46:30 -0500

    Add invisible recaptcha

--- src/pages/join/JoinForm.js
+++ src/pages/join/JoinForm.js
@@ -1,25 +1,50 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, getFormValues } from 'redux-form';
+import Recaptcha from 'react-recaptcha';
 
+import config from 'src/config';
 import { LINKS } from 'src/constants';
 import { TextFieldWrapper, CheckboxWrapper } from 'src/components/reduxFormWrappers';
 import { Button, UnstyledLink, Grid, Label } from '@sparkpost/matchbox';
 import { required, minLength, email } from 'src/helpers/validation';
 import styles from './JoinPage.module.scss';
+const { recaptcha } = config;
 
+const formName = 'registerAccountForm';
+
+let reCaptchaInstance;
 export class JoinForm extends Component {
+  state = {
+    reCaptchaReady: false
+  }
+
+  executeRecaptcha = (values) => {
+    reCaptchaInstance.execute();
+  }
+
+  reCaptchaLoaded = () => {
+    this.setState({ reCaptchaReady: true });
+  }
+
+  verifyCallback = (response) => {
+    const { formValues, onSubmit } = this.props;
+    reCaptchaInstance.reset();
+    onSubmit({ ...formValues, recaptcha_response: response, recaptcha_type: 'invisible' });
+  }
 
   render() {
     const {
-      handleSubmit,
       submitting,
       pristine,
       invalid
     } = this.props;
 
+    const { reCaptchaReady } = this.state;
+
     return (
-      <form onSubmit={handleSubmit}>
+      <form>
         <Grid className={styles.spacer}>
           <Grid.Column xs={12} md={6} lg={6}>
             <Field
@@ -27,7 +52,7 @@ export class JoinForm extends Component {
               component={TextFieldWrapper}
               label='First Name'
               validate={required}
-              disabled={false}
+              disabled={!reCaptchaReady || submitting}
             />
           </Grid.Column>
           <Grid.Column xs={12} md={6} lg={6}>
@@ -36,7 +61,7 @@ export class JoinForm extends Component {
               component={TextFieldWrapper}
               label='Last Name'
               validate={required}
-              disabled={false}
+              disabled={!reCaptchaReady || submitting}
             />
           </Grid.Column>
         </Grid>
@@ -45,25 +70,25 @@ export class JoinForm extends Component {
           component={TextFieldWrapper}
           label='Email'
           validate={[required, email]}
-          disabled={false}
+          disabled={!reCaptchaReady || submitting}
+          autoComplete='username email'
         />
         <Field
           name='password'
           component={TextFieldWrapper}
           label='Password'
           validate={[required, minLength(8)]}
-          disabled={false}
+          disabled={!reCaptchaReady || submitting}
           type='password'
           autoComplete='new-password'
-          // data-lpignore={true} // removes inaccurate LastPass password management behavior
         />
         <div className={styles.inline}>
           <Field
-            name='tou_accepted'
+            name="tou_accepted"
             id='tou_accepted'
-            label=''
-            validate={required}
             component={CheckboxWrapper}
+            type="checkbox"
+            disabled={!reCaptchaReady || submitting}
           />
           <div className={styles.touLabel}>
             <Label id='tou_accepted'>
@@ -72,15 +97,34 @@ export class JoinForm extends Component {
           </div>
         </div>
 
-        <Button primary submit disabled={submitting || pristine || invalid }>{ submitting ? 'Loading' : 'Create Account' }</Button>
+        <Button primary
+          disabled={submitting || pristine || invalid || !reCaptchaReady }
+          onClick={this.executeRecaptcha}
+          id='g-recaptcha'
+        >
+          { submitting ? 'Loading' : 'Create Account' }
+        </Button>
+
+        <Recaptcha
+          ref={(e) => reCaptchaInstance = e}
+          sitekey={recaptcha.invisibleKey}
+          size="invisible"
+          render='explicit'
+          onloadCallback={this.reCaptchaLoaded}
+          verifyCallback={this.verifyCallback}
+        />
+
       </form>
     );
   }
 }
 
 const mapStateToProps = (state, props) => ({
-  initialValues: { }
+  initialValues: {
+    tou_accepted: false
+  },
+  formValues: getFormValues(formName)(state)
 });
 
-const RegisterUserReduxForm = reduxForm({ form: 'RegisterUser' })(JoinForm);
-export default connect(mapStateToProps, {})(RegisterUserReduxForm);
+const RegisterAccountForm = reduxForm({ form: formName })(JoinForm);
+export default connect(mapStateToProps, {})(RegisterAccountForm);
--- src/pages/join/JoinPage.js
+++ src/pages/join/JoinPage.js
@@ -1,9 +1,11 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import qs from 'query-string';
 import cookie from 'js-cookie';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { SparkPost } from 'src/components';
 import { Panel, UnstyledLink } from '@sparkpost/matchbox';
@@ -16,6 +18,15 @@ import { DEFAULT_REDIRECT_ROUTE, LINKS } from 'src/constants';
 const { attribution, salesforceDataParams } = config;
 
 export class JoinPage extends Component {
+
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js';
+    script.async = true;
+    document.body.appendChild(script); //TODO find better way of doing this in react. 
+
+    this.props.logout();
+  }
 
   getAndSetAttributionData = () => {
     const { params } = this.props;
@@ -36,32 +47,32 @@ export class JoinPage extends Component {
   }
 
   handleSignupFailure = (errors) => {
-    // console.error(errors);
+    console.error(errors);
   }
 
   registerSubmit = (values) => {
     //TODO
-    //- retrieve salesforce_data
-    //- retrieve sfdcid
+    //- retrieve salesforce_data - DONE
+    //- retrieve sfdcid - DONE
     //- retrieve analytics session id
-    //- handle and retrieve recaptcha
+    //- handle and retrieve recaptcha - DONE
     //- track signup after signup is completed
     //- fix TOU checkbox alignment when validation error is shown
+    //- handle recaptcha lib loading on join page only
 
-
+    console.log('received', values);
     const { register } = this.props;
     const attributionData = this.getAndSetAttributionData();
 
     const signupData = { ...values, salesforce_data: attributionData, sfdcid: attributionData.sfdcid };
 
+    /*
+    {"results":{"message":"Account created, welcome to SparkPost!","customer_id":209744,"username":"mohammad-hossain-8613"}}
+    */
     return register(signupData)
       .then((accountData) => this.props.authenticate(accountData.username, values.password))
       .then(() => this.props.history.push(DEFAULT_REDIRECT_ROUTE))
       .catch(this.handleSignupFailure);
-  }
-
-  componentDidMount() {
-    this.props.logout();
   }
 
   render() {
@@ -76,6 +87,7 @@ export class JoinPage extends Component {
         <Panel sectioned accent title="Log In">
           <JoinForm onSubmit={this.registerSubmit} />
         </Panel>
+        Already have an account? <Link to={'/auth'}>Log In</Link>.
       </div>
     );
   }
