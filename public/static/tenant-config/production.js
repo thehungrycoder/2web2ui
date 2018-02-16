c24d28802678366d03046008ecf52499461c8ac4
Brian Kemper <brian.kemper@sparkpost.com>
2018-02-16 16:04:03 -0500

    FAD-6109 Dev configuration for Sift Science

--- public/static/tenant-config/production.js
+++ public/static/tenant-config/production.js
@@ -7,6 +7,9 @@ window.SP.productionConfig = {
     has_signup: true
   },
   gaTag: 'UA-111136819-2',
+  siftScience: {
+    id: '88affa8e11'
+  },
   splashPage: '/dashboard',
   smtpAuth: {
     host: 'smtp.sparkmail.com',
