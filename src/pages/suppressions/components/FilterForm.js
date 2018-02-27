2c3dd6018338f94df417b012c8c03950e8ecc5bf
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-02-26 22:22:45 -0500

    FAD-6284 updated suppression date refresh flow

--- src/pages/suppressions/components/FilterForm.js
+++ src/pages/suppressions/components/FilterForm.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-
 import { reduxForm } from 'redux-form';
 import { Grid } from '@sparkpost/matchbox';
 import { FilterDropdown } from 'src/components';
 import DateFilter from 'src/pages/reports/components/DateFilter';
-import { getRelativeDates } from 'src/helpers/date';
+import { refreshReportOptions } from 'src/actions/reportOptions';
 import styles from './FilterForm.module.scss';
 
 const types = [
@@ -46,35 +45,11 @@ const sources = [
 ];
 
 export class FilterForm extends Component {
-  constructor(props) {
-    super(props);
 
-    const { reportOptions } = props;
-    this.state = {
-      reportOptions,
-      types: [],
-      sources: []
-    };
-  }
-
-  refresh() {
-    this.props.onSubmit(this.state);
-  }
-
-  handleDateSelection = (options) => {
-    const { relativeRange } = options;
-    if (relativeRange) {
-      Object.assign(options, getRelativeDates(relativeRange));
-    }
-
-    this.setState({
-      reportOptions: {
-        from: options.from,
-        to: options.to,
-        relativeRange: relativeRange
-      }
-    });
-  }
+  state = {
+    types: [],
+    sources: []
+  };
 
   handleTypesSelection = (selected) => {
     const values = _.compact(_.map(selected, (val, key) => val ? key : undefined));
@@ -95,17 +70,14 @@ export class FilterForm extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (!_.isEqual(this.state, prevState)) {
-      this.refresh();
+    const { reportOptions } = this.props;
+    if (this.state !== prevState || reportOptions !== prevProps.reportOptions) {
+      this.props.onSubmit({ ...this.state, reportOptions });
     }
   }
 
   componentDidMount() {
-    const { list } = this.props;
-    // Note: This will cause API request each time this section is visited but that's ok
-    if (list && !list.length) {
-      this.refresh();
-    }
+    this.props.refreshReportOptions({ force: true });
   }
 
   render() {
@@ -113,7 +85,7 @@ export class FilterForm extends Component {
       <Grid>
         <Grid.Column xs={12} md={6}>
           <div>
-            <DateFilter refresh={this.handleDateSelection} />
+            <DateFilter />
           </div>
         </Grid.Column>
         <Grid.Column xs={6} md={3}>
@@ -146,4 +118,4 @@ const mapStateToProps = (state) => ({
   reportOptions: state.reportOptions,
   list: state.suppressions.list
 });
-export default connect(mapStateToProps)(reduxForm(formOptions)(FilterForm));
+export default connect(mapStateToProps, { refreshReportOptions })(reduxForm(formOptions)(FilterForm));
