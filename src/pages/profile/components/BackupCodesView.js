6c0713349b459d47b185a241eae10e31476052cd
Jon Ambas <jon@jonambas.com>
2018-02-28 12:38:16 -0500

    FAD-6245 Update Matchbox & Small ui fixes

--- src/pages/profile/components/BackupCodesView.js
+++ src/pages/profile/components/BackupCodesView.js
@@ -3,14 +3,17 @@ import BackupCodesList from './BackupCodesList';
 import DownloadCodes from './DownloadCodes';
 import CopyCodes from './CopyCodes';
 import PrintCodes from './PrintCodes';
+import { Button } from '@sparkpost/matchbox';
 
 const BackupCodes = ({ codes }) => (
   <div>
     <p><strong>Your shiny new backup codes:</strong></p>
-    <BackupCodesList codes={codes} />
-    <DownloadCodes codes={codes} />
-    <CopyCodes codes={codes} />
-    <PrintCodes codes={codes} />
+    <Button.Group>
+      <BackupCodesList codes={codes} />
+      <DownloadCodes codes={codes} />
+      <CopyCodes codes={codes} />
+      <PrintCodes codes={codes} />
+    </Button.Group>
   </div>
 );
 
