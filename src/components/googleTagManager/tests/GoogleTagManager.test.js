01465fe8cd0d1a417723c1647f6ffc6521975699
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-04-02 11:08:36 -0400

    FAD-6465 tweaked content-view event submission for GTM for public routes

--- src/components/googleTagManager/tests/GoogleTagManager.test.js
+++ src/components/googleTagManager/tests/GoogleTagManager.test.js
@@ -11,7 +11,7 @@ describe('Component: GoogleTagManager', () => {
   let props;
 
   beforeEach(() => {
-    findRouteByPath.default = jest.fn(() => ({ title: 'Test Title' }));
+    findRouteByPath.default = jest.fn(() => ({ title: 'Test Title', public: false }));
     props = {
       id: 'GTM-XXXX',
       location: {
@@ -29,6 +29,7 @@ describe('Component: GoogleTagManager', () => {
   });
 
   it('should push the username and track the initial event when username becomes available', () => {
+    expect(window.dataLayer).toEqual([]);
     wrapper.setProps({ username: 'jojo' });
     expect(window.dataLayer).toEqual([
       {
@@ -40,6 +41,22 @@ describe('Component: GoogleTagManager', () => {
     ]);
   });
 
+  it('should track a page view immediately if on a public route, and not again when the username becomes available', () => {
+    findRouteByPath.default = jest.fn(() => ({ title: 'Test Public Title', public: true }));
+    wrapper = shallow(<GoogleTagManager {...props} />);
+    expect(window.dataLayer).toEqual([
+      {
+        event: 'content-view',
+        'content-name': '/some/path?fun=times',
+        'content-title': 'Test Public Title',
+        username: undefined
+      }
+    ]);
+
+    wrapper.setProps({ username: 'loaded' });
+    expect(window.dataLayer).toHaveLength(1);
+  });
+
   it('should track a new page view when the location changes', () => {
     findRouteByPath.default = jest.fn(() => ({ title: 'New Title' }));
     wrapper.setProps({ username: 'fred', location: { pathname: '/new', search: '' }});
@@ -49,6 +66,11 @@ describe('Component: GoogleTagManager', () => {
       'content-title': 'New Title',
       username: 'fred'
     });
+  });
+
+  it('should not track a new page view when the location changes if we are still waiting for the username to load', () => {
+    wrapper.setProps({ location: { pathname: '/new2_FINAL', search: '' }});
+    expect(window.dataLayer).toEqual([]);
   });
 
   it('should use the pathname if no title is available', () => {
