910b2fd1644740db5fd76a0d19a8114bd8149374
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-04-11 17:32:32 -0400

    FAD-6493 added babel-polyfill for IE11 support

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -80,7 +80,7 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: ['babel-polyfill', require.resolve('./polyfills'), paths.appIndexJs],
   output: {
     // The build folder.
     path: paths.appBuild,
