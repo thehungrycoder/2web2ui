c0031a670f6aee6a4b3a87027ff236456149f478
Jon Ambas <jon@jonambas.com>
2018-04-13 13:41:40 -0400

    FAD-6478 Update tests

---
+++ src/components/modals/tests/Modal.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Modal from '../Modal';
+import { shallow } from 'enzyme';
+
+describe('Modal Component', () => {
+  let wrapper;
+
+  const props = {
+    open: false,
+    onClose: jest.fn()
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<Modal {...props}>content</Modal>);
+  });
+
+  it('should not render contents when not open', () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Modal').dive().find('TransitionGroup').children()).toHaveLength(0);
+  });
+
+  it('should render contents when not open', () => {
+    wrapper.setProps({ open: true });
+    expect(wrapper.find('Modal').dive().find('TransitionGroup').children()).toMatchSnapshot();
+  });
+});
