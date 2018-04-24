504d3755747fbe468a3ed0a235f6ef0a0d3f99e8
Jon Ambas <jon@jonambas.com>
2018-04-24 14:50:08 -0400

    FE-294 Update prototype to include status and inv #

--- src/pages/billing/components/InvoiceHistory.js
+++ src/pages/billing/components/InvoiceHistory.js
@@ -1,12 +1,42 @@
 import React, { Fragment } from 'react';
-import { Panel } from '@sparkpost/matchbox';
-import { Collection } from 'src/components';
+import { Panel, Button, Icon } from '@sparkpost/matchbox';
+import { TableCollection } from 'src/components';
 import { formatDate } from 'src/helpers/date';
 import _ from 'lodash';
 
-const data = _.fill(Array(20), { date: new Date(), amount: 9123.333 });
+import styles from './InvoiceHistory.module.scss';
+
+const data = _.fill(Array(8), {
+  date: new Date(),
+  amount: 9123.33,
+  invoiceNumber: 'INV00000002',
+  // Posted | Draft | Canceled | Error - unsure what these mean, remove icons if this doesnt make sense
+  status: 'Posted'
+});
+
+const columns = [
+  { label: null, width: '18px' },
+  'Date',
+  'Amount',
+  'Invoice Number',
+  null
+];
+
+const getRowData = ({ status, date, amount, invoiceNumber }) => ([
+  status === 'Posted'
+    ? <Icon name='Check' className={styles.Posted} size={20} />
+    : <Icon name='Close' className={styles.Error} size={20} />,
+  formatDate(date),
+  formatCurrency(amount),
+  invoiceNumber,
+  <div style={{ textAlign: 'right' }}><Button plain size='small'>Download</Button></div>
+]);
+
+const formatCurrency = (v) => `$${v.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 
 const InvoiceHistory = ({ invoices = data }) => {
+  // Perhaps need a "amounts shown in USD" message somewhere?
+
   const maxWarning = invoices.length === 20
     ? <Panel.Footer left={<p><small>Only your last 20 invoices are available to be viewed</small></p>} />
     : null;
@@ -14,23 +44,20 @@ const InvoiceHistory = ({ invoices = data }) => {
   return (
     <Fragment>
       <Panel title='Invoice History'>
-        <Collection rows={invoices} rowComponent={Invoice}/>
+        <TableCollection rows={invoices} columns={columns} getRowData={getRowData} />
       </Panel>
       {maxWarning}
     </Fragment>
   );
 };
 
-const formatCurrency = (v) => `$${v.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
-
-const Invoice = ({ date, amount }) => (
-  <Panel.Section actions={[{ content: 'Download' }]}>
-    {/*
-      this looks okay too? ¯\_(ツ)_/¯ <LabelledValue label={formatDate(date)} value={<Fragment>${amount.toLocaleString()}</Fragment>} />
-    */}
-    <h6 style={{ marginBottom: 0 }}><strong>{formatCurrency(amount)}</strong></h6>
-    <small>{formatDate(date)}</small>
-  </Panel.Section>
-);
+// In case 'status' & 'invoiceNumber' don't work out
+// <Collection rows={invoices} rowComponent={Invoice}/>
+// const Invoice = ({ date, amount, invoiceNumber }) => (
+//   <Panel.Section actions={[{ content: 'Download' }]}>//
+//     <h6 style={{ marginBottom: 0 }}><strong>{formatCurrency(amount)}</strong></h6>
+//     <small>{formatDate(date)}</small>
+//   </Panel.Section>
+// );
 
 export default InvoiceHistory;
