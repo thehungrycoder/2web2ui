7da9d7b09608fdd7b972289ae5b5786fddf1c872
Ewan Dennis <ewan.dennis@sparkpost.com>
2018-04-26 15:08:11 +0100

    FE-304: added tracking helper tests

---
+++ src/helpers/tests/conversionTracking.test.js
@@ -0,0 +1,65 @@
+import * as analytics from 'src/helpers/analytics';
+import * as conversions from 'src/helpers/conversionTracking';
+
+jest.mock('src/helpers/analytics');
+
+describe('Conversion tracking', () => {
+  describe('trackPlanChange', () => {
+    const plans = [
+      { code: '27', volume: 27 },
+      { code: '257', volume: 257 }
+    ];
+
+    it('should emit trackFormSuccess', () => {
+      conversions.trackPlanChange(plans, '27', '257');
+      expect(analytics.trackFormSuccess).toHaveBeenCalled();
+    });
+
+    it('should emit upgrade when the new plan has higher volume', () => {
+      conversions.trackPlanChange(plans, '27', '257');
+      expect(analytics.trackFormSuccess).toHaveBeenCalledWith('upgrade', {
+        form_type: 'upgrade',
+        plan_key: '257'
+      });
+    });
+
+    it('should emit downgrade when the new plan has lower or equal volume', () => {
+      conversions.trackPlanChange(plans, '257', '27');
+      expect(analytics.trackFormSuccess).toHaveBeenCalledWith('downgrade', {
+        form_type: 'downgrade',
+        plan_key: '27'
+      });
+    });
+  });
+
+  describe('trackAddonPurchase', () => {
+    const addon = 'Shiny Addonington';
+
+    beforeEach(() => {
+      conversions.trackAddonPurchase(addon);
+    });
+
+    it('should emit trackFormSuccess', () => {
+      expect(analytics.trackFormSuccess).toHaveBeenCalled();
+    });
+
+    it('should set event action and data', () => {
+      expect(analytics.trackFormSuccess).toHaveBeenCalledWith('purchase_addon', { form_type: 'purchase_addon', addon_key: addon });
+    });
+  });
+
+  describe('trackAddonRequest', () => {
+    const addon = 'Gimme De-Thang';
+    beforeEach(() => {
+      conversions.trackAddonRequest(addon);
+    });
+
+    it('should emit trackFormSuccess', () => {
+      expect(analytics.trackEvent).toHaveBeenCalled();
+    });
+
+    it('should set event, action and data', () => {
+      expect(analytics.trackEvent).toHaveBeenCalledWith('Clicked button', addon, { action: addon });
+    });
+  });
+});
