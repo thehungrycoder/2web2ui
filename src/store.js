6fd2f8832512f556b589c218e3d3af39cfe790d8
Jon Ambas <jon@jonambas.com>
2018-04-27 13:50:47 -0400

    FE-308 Enable hot module replacement

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import ErrorTracker from './helpers/errorTracker';
+
+const configureStore = () => {
+  // necessary for redux devtools in development mode only
+  const composeEnhancers = process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line no-mixed-operators
+
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(
+      applyMiddleware(thunk),
+      applyMiddleware(ErrorTracker.middleware)
+    )
+  );
+
+  // Hot module replacement
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('./reducers', () => {
+        store.replaceReducer(rootReducer);
+      });
+    }
+  }
+
+  return store;
+};
+
+export default configureStore;
