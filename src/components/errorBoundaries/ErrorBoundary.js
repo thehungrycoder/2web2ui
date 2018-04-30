e36844c557ad375d78e4d932acf6c4410c1feccf
mhossain <mohammad.hossain@sparkpost.com>
2018-04-30 15:20:43 -0400

    fix positioning

--- src/components/errorBoundaries/ErrorBoundary.js
+++ src/components/errorBoundaries/ErrorBoundary.js
@@ -21,13 +21,16 @@ export default class ErrorBoundary extends Component {
 
   render () {
     if (this.state.hasError) {
-      return <EmptyState
-        title='Sorry, something went wrong'
-        image='Generic'
-        primaryAction={primaryAction}
-      >
-        <p>We're having some technical issues. Our engineers have been notified and are working on getting this fixed.</p>
-      </EmptyState>;
+      return <div style={{ margin: '0 auto', maxWidth: 1080 }}>
+        <EmptyState
+          title='Sorry, something went wrong'
+          image='Generic'
+          primaryAction={primaryAction}
+        >
+          <p>We're having some technical issues. Our engineers have been notified and are working on getting this
+            fixed.</p>
+        </EmptyState>
+      </div>;
     }
 
     return this.props.children;
