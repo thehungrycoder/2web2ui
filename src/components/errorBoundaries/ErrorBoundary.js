7560fcb6dba62cab8f7bba8d5b0e5d2bb056b07b
Jon Ambas <jon@jonambas.com>
2018-05-03 15:19:49 -0400

    FE-143 Update error boundary empty state image

--- src/components/errorBoundaries/ErrorBoundary.js
+++ src/components/errorBoundaries/ErrorBoundary.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { EmptyState } from '@sparkpost/matchbox';
+import { Generic } from 'src/components/images';
 import ErrorTracker from 'src/helpers/errorTracker';
 
 const primaryAction = {
@@ -24,7 +25,7 @@ export default class ErrorBoundary extends Component {
       return <div style={{ margin: '0 auto', maxWidth: 1080 }}>
         <EmptyState
           title='Sorry, something went wrong'
-          image='Generic'
+          image={Generic}
           primaryAction={primaryAction}
         >
           <p>We're having some technical issues. Our engineers have been notified and are working on getting this
