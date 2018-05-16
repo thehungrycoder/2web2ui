03b029c66db3630d9409fc4e225773221120504a
Bob Evans <robert.evans25@gmail.com>
2018-05-16 17:51:35 -0400

    AC-106 fixed issue with resetting error on pending password reset

--- src/reducers/passwordReset.js
+++ src/reducers/passwordReset.js
@@ -10,7 +10,7 @@ const initialState = {
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'SEND_PASSWORD_EMAIL_PENDING':
-      return { ...state, emailPending: true, emailSuccess: false };
+      return { ...state, emailPending: true, emailSuccess: false, emailError: null };
 
     case 'SEND_PASSWORD_EMAIL_SUCCESS':
       return { ...state, error: null, emailPending: false, emailSuccess: true };
