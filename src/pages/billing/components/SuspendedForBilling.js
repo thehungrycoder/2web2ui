a7612548e1371e08beb41c1429edbd7810c01c3c
Brian Kemper <brian.kemper@sparkpost.com>
2018-05-18 10:54:39 -0400

    FE-93 Changed suspended for billing warning to include link to open support ticket

--- src/pages/billing/components/SuspendedForBilling.js
+++ src/pages/billing/components/SuspendedForBilling.js
@@ -1,8 +1,7 @@
-
 import React, { Fragment } from 'react';
-import config from 'src/config';
-import { Banner, UnstyledLink } from '@sparkpost/matchbox';
+import { Banner } from '@sparkpost/matchbox';
 import UpdatePaymentForm from '../forms/UpdatePayment';
+import SupportTicketLink from 'src/components/supportTicketLink/SupportTicketLink';
 
 export default function SuspendedForBilling({ account }) {
   const { billing = {}} = account;
@@ -10,8 +9,18 @@ export default function SuspendedForBilling({ account }) {
   return (
     <Fragment>
       <Banner status='danger' title='Your account has been suspended due to a billing problem' >
-        <p>We sent an email notification to your current billing contact email address{email}. To reactivate your account and pay your outstanding balance due, please update your payment information below.</p>
-        <p>If you have any questions, please <UnstyledLink to={`mailto:${config.contact.billingEmail}`}>contact us</UnstyledLink>.</p>
+        <p>
+          We sent an email notification to your current billing contact email address {email}. To
+          reactivate your account and pay your outstanding balance due, please update your payment
+          information below.
+        </p>
+        <p>
+          If this is incorrect, please {
+            <SupportTicketLink issueId="account_suspension">
+              submit a support ticket
+            </SupportTicketLink>
+          } and let us know why.
+        </p>
       </Banner>
       <UpdatePaymentForm />
     </Fragment>
