fe7f06d4470ce59f31963387bcf93805b658b840
Brian Kemper <brian.kemper@sparkpost.com>
2018-05-24 13:40:30 -0400

    FE-92 Support hidden defaultRefinement for AlgoliaSearch

--- src/components/support/components/AlgoliaSearch.js
+++ src/components/support/components/AlgoliaSearch.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import { TextField } from '@sparkpost/matchbox';
 import { connectSearchBox } from 'react-instantsearch/connectors';
 
-export const AlgoliaSearch = ({ currentRefinement, refine }) =>
-  <TextField
-    onChange={(e) => refine(e.currentTarget.value)}
-    value={currentRefinement}
-    placeholder={'Have a question?  Ask or enter a search term here.'}
+export class AlgoliaSearch extends Component {
+  // Must track user entered text to control the display
+  state = {
+    text: ''
+  }
 
-  />;
+  handleChange = ({ currentTarget }) => {
+    const nextText = currentTarget.value;
+
+    // Use default refinement when a search term is not provided
+    const nextRefinement = _.trim(nextText) === ''
+      ? this.props.defaultRefinement
+      : nextText;
+
+    this.setState({ text: nextText });
+    this.props.refine(nextRefinement);
+  };
+
+  render() {
+    return (
+      <TextField
+        onChange={this.handleChange}
+        placeholder="Have a question?  Ask or enter a search term here."
+        value={this.state.text}
+      />
+    );
+  }
+}
 
 export default connectSearchBox(AlgoliaSearch);
-
--- src/components/support/components/tests/AlgoliaSearch.test.js
+++ src/components/support/components/tests/AlgoliaSearch.test.js
@@ -4,23 +4,37 @@ import { shallow } from 'enzyme';
 import { AlgoliaSearch } from '../AlgoliaSearch';
 
 describe('Algolia Search component', () => {
-  let wrapper;
+  const defaultRefinement = 'default refinement';
+  const inputText = 'example text';
   let props;
+  let wrapper;
+
   beforeEach(() => {
     props = {
-      currentRefinement: 'my text',
+      defaultRefinement,
       refine: jest.fn()
     };
-
     wrapper = shallow(<AlgoliaSearch {...props} />);
   });
 
-  it('should render', () => {
+  it('should render with empty text field', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should call refine on change', () => {
-    wrapper.find('TextField').simulate('change', { currentTarget: { value: 'foo' }});
-    expect(props.refine).toHaveBeenCalledWith('foo');
+  it('should render with input text', () => {
+    wrapper.setState({ text: inputText });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render and refine with input text', () => {
+    wrapper.find('TextField').simulate('change', { currentTarget: { value: inputText }});
+    expect(props.refine).toHaveBeenCalledWith(inputText);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render with input text and refine with default refinement', () => {
+    wrapper.find('TextField').simulate('change', { currentTarget: { value: ' ' }});
+    expect(props.refine).toHaveBeenCalledWith(defaultRefinement);
+    expect(wrapper).toMatchSnapshot();
   });
 });
