7fa1a6696560c58d7aa22bf00048583683bac26f
Brian Kemper <brian.kemper@sparkpost.com>
2018-05-25 12:24:44 -0400

    FE-92 Defined a keyword for each route

--- src/config/routes.js
+++ src/config/routes.js
@@ -168,10 +168,13 @@ const routes = [
     condition: all(
       hasGrants('api_keys/manage', 'templates/modify', 'sending_domains/manage'),
       configEquals('splashPage', '/dashboard') // want to hide if not a splash page https://jira.int.messagesystems.com/browse/FAD-6046
-    )
+    ),
     // TODO: implement some kind of blockedRoutes check that runs on every route so we can
     // hide routes based on config, account/user settings, etc. without having to mess
     // around with grants in the web UI keys
+    keywords: [
+      'started'
+    ]
   },
   {
     path: '/reports',
@@ -181,49 +184,79 @@ const routes = [
     path: '/reports/summary',
     component: reports.SummaryPage,
     layout: App,
-    title: 'Summary Report'
+    title: 'Summary Report',
+    keywords: [
+      'reporting'
+    ]
   },
   {
     path: '/reports/bounce',
     component: reports.BouncePage,
     layout: App,
-    title: 'Bounce Report'
+    title: 'Bounce Report',
+    keywords: [
+      'bounce',
+      'reporting'
+    ]
   },
   {
     path: '/reports/rejections',
     component: reports.RejectionPage,
     layout: App,
-    title: 'Rejection Report'
+    title: 'Rejection Report',
+    keywords: [
+      'reject',
+      'reporting'
+    ]
   },
   {
     path: '/reports/accepted',
     component: reports.AcceptedPage,
     layout: App,
-    title: 'Accepted Report'
+    title: 'Accepted Report',
+    keywords: [
+      'accept',
+      'reporting'
+    ]
   },
   {
     path: '/reports/delayed',
     component: reports.DelayPage,
     layout: App,
-    title: 'Delayed Report'
+    title: 'Delayed Report',
+    keywords: [
+      'delay',
+      'reporting'
+    ]
   },
   {
     path: '/reports/engagement',
     component: reports.EngagementPage,
     layout: App,
-    title: 'Engagement Report'
+    title: 'Engagement Report',
+    keywords: [
+      'engagement',
+      'reporting'
+    ]
   },
   {
     path: '/reports/message-events',
     component: reports.MessageEventsPage,
     layout: App,
-    title: 'Message Events Report'
+    title: 'Message Events Report',
+    keywords: [
+      'event',
+      'reporting'
+    ]
   },
   {
     path: '/reports/message-events/details/:messageId',
     component: reports.EventPage,
     layout: App,
-    title: 'Message History'
+    title: 'Message History',
+    keywords: [
+      'event'
+    ]
   },
   {
     path: '/account/security',
@@ -232,21 +265,30 @@ const routes = [
   {
     path: '/account/email-verification/:token',
     component: emailVerification,
-    title: 'Verify Your Email'
+    title: 'Verify Your Email',
+    keywords: [
+      'verification'
+    ]
   },
   {
     path: '/account/subaccounts',
     component: subaccounts.ListPage,
     condition: hasGrants('subaccount/manage', 'api_keys/manage', 'sending_domains/manage'),
     layout: App,
-    title: 'Subaccounts'
+    title: 'Subaccounts',
+    keywords: [
+      'subaccout'
+    ]
   },
   {
     path: '/account/subaccounts/create',
     component: subaccounts.CreatePage,
     condition: hasGrants('subaccount/manage', 'api_keys/manage', 'sending_domains/manage'),
     layout: App,
-    title: 'New Subaccount'
+    title: 'New Subaccount',
+    keywords: [
+      'subaccout'
+    ]
   },
   {
     path: '/account/subaccounts/:id',
@@ -254,112 +296,160 @@ const routes = [
     condition: hasGrants('subaccount/manage', 'api_keys/manage', 'sending_domains/manage'),
     layout: App,
     title: 'Subaccount Details',
-    exact: false
+    exact: false,
+    keywords: [
+      'subaccout'
+    ]
   },
   {
     path: '/account/users',
     component: users.ListPage,
     condition: hasGrants('users/manage'),
     layout: App,
-    title: 'Users'
+    title: 'Users',
+    keywords: [
+      'user'
+    ]
   },
   {
     path: '/account/users/create',
     component: users.CreatePage,
     condition: hasGrants('users/manage'),
     layout: App,
-    title: 'Invite User'
+    title: 'Invite User',
+    keywords: [
+      'user'
+    ]
   },
   {
     path: '/templates',
     component: templates.ListPage,
     condition: hasGrants('templates/view'),
     layout: App,
-    title: 'Templates'
+    title: 'Templates',
+    keywords: [
+      'template'
+    ]
   },
   {
     path: '/templates/create/:id?',
     component: templates.CreatePage,
     condition: hasGrants('templates/modify'),
     layout: App,
-    title: 'New Template'
+    title: 'New Template',
+    keywords: [
+      'template'
+    ]
   },
   {
     path: '/templates/edit/:id',
     component: templates.EditPage,
     condition: hasGrants('templates/view'),
     layout: App,
-    title: 'Edit Template'
+    title: 'Edit Template',
+    keywords: [
+      'template'
+    ]
   },
   {
     path: '/templates/edit/:id/published',
     component: templates.PublishedPage,
     condition: hasGrants('templates/view'),
     layout: App,
-    title: 'View Published Template'
+    title: 'View Published Template',
+    keywords: [
+      'template'
+    ]
   },
   {
     path: '/templates/preview/:id',
     component: templates.PreviewDraftPage,
     condition: hasGrants('templates/view'),
     layout: App,
-    title: 'Preview Draft Template'
+    title: 'Preview Draft Template',
+    keywords: [
+      'template'
+    ]
   },
   {
     path: '/templates/preview/:id/published',
     component: templates.PreviewPublishedPage,
     condition: hasGrants('templates/view'),
     layout: App,
-    title: 'Preview Published Template'
+    title: 'Preview Published Template',
+    keywords: [
+      'template'
+    ]
   },
   {
     path: '/lists/recipient-lists',
     component: recipientLists.ListPage,
     condition: hasGrants('recipient_lists/manage'),
     layout: App,
-    title: 'Recipient Lists'
+    title: 'Recipient Lists',
+    keywords: [
+      'recipient list'
+    ]
   },
   {
     path: '/lists/recipient-lists/create',
     component: recipientLists.CreatePage,
     condition: hasGrants('recipient_lists/manage'),
     layout: App,
-    title: 'New Recipient List'
+    title: 'New Recipient List',
+    keywords: [
+      'recipient list'
+    ]
   },
   {
     path: '/lists/recipient-lists/edit/:id',
     component: recipientLists.EditPage,
     condition: hasGrants('recipient_lists/manage'),
     layout: App,
-    title: 'Edit Recipient List'
+    title: 'Edit Recipient List',
+    keywords: [
+      'recipient list'
+    ]
   },
   {
     path: '/lists/suppressions',
     component: suppressions.ListPage,
     condition: hasGrants('suppression_lists/manage'),
     layout: App,
-    title: 'Suppression List'
+    title: 'Suppression List',
+    keywords: [
+      'suppression list'
+    ]
   },
   {
     path: '/lists/suppressions/create',
     component: suppressions.CreatePage,
     condition: hasGrants('suppression_lists/manage'),
     layout: App,
-    title: 'New Suppression'
+    title: 'New Suppression',
+    keywords: [
+      'suppression list'
+    ]
   },
   {
     path: '/webhooks',
     component: webhooks.ListPage,
     condition: hasGrants('webhooks/view'),
     layout: App,
-    title: 'Webhooks'
+    title: 'Webhooks',
+    keywords: [
+      'webhook'
+    ]
   },
   {
     path: '/webhooks/create',
     component: webhooks.CreatePage,
     condition: hasGrants('webhooks/modify'),
     layout: App,
-    title: 'New Webhook'
+    title: 'New Webhook',
+    keywords: [
+      'webhook'
+    ]
   },
   {
     path: '/webhooks/details/:id',
@@ -367,156 +457,225 @@ const routes = [
     condition: hasGrants('webhooks/modify'),
     layout: App,
     title: 'Webhook Details',
-    exact: false
+    exact: false,
+    keywords: [
+      'webhook'
+    ]
   },
   {
     path: '/account/api-keys',
     component: apiKeys.ListPage,
     condition: hasGrants('api_keys/manage'),
     layout: App,
-    title: 'API Keys'
+    title: 'API Keys',
+    keywords: [
+      'api key'
+    ]
   },
   {
     path: '/account/api-keys/create',
     component: apiKeys.CreatePage,
     condition: hasGrants('api_keys/manage'),
     layout: App,
-    title: 'New API Key'
+    title: 'New API Key',
+    keywords: [
+      'api key'
+    ]
   },
   {
     path: '/account/api-keys/edit/:id',
     component: apiKeys.EditPage,
     condition: hasGrants('api_keys/manage'),
     layout: App,
-    title: 'Edit API Key'
+    title: 'Edit API Key',
+    keywords: [
+      'api key'
+    ]
   },
   {
     path: '/account/api-keys/view/:id',
     component: apiKeys.DetailsPage,
     condition: hasGrants('api_keys/manage'),
     layout: App,
-    title: 'View API Key'
+    title: 'View API Key',
+    keywords: [
+      'api key'
+    ]
   },
   {
     path: '/account/tracking-domains',
     component: trackingDomains.ListPage,
     condition: hasGrants('tracking_domains/view'),
     layout: App,
-    title: 'Tracking Domains'
+    title: 'Tracking Domains',
+    keywords: [
+      'tracking domain'
+    ]
   },
   {
     path: '/account/tracking-domains/create',
     component: trackingDomains.CreatePage,
     condition: hasGrants('tracking_domains/manage'),
     layout: App,
-    title: 'New Tracking Domain'
+    title: 'New Tracking Domain',
+    keywords: [
+      'tracking domain'
+    ]
   },
   {
     path: '/account/profile',
     component: emailVerificationRedirect,
     condition: hasGrants('users/self-manage'),
     layout: App,
-    title: 'My Profile'
+    title: 'My Profile',
+    keywords: [
+      'account profile'
+    ]
   },
   {
     path: '/account/sending-domains',
     component: sendingDomains.ListPage,
     condition: hasGrants('sending_domains/manage'),
     layout: App,
-    title: 'Sending Domains'
+    title: 'Sending Domains',
+    keywords: [
+      'sending domain'
+    ]
   },
   {
     path: '/account/sending-domains/create',
     component: sendingDomains.CreatePage,
     condition: hasGrants('sending_domains/manage'),
     layout: App,
-    title: 'New Sending Domain'
+    title: 'New Sending Domain',
+    keywords: [
+      'sending domain'
+    ]
   },
   {
     path: '/account/sending-domains/edit/:id',
     component: sendingDomains.EditPage,
     condition: hasGrants('sending_domains/manage'),
     layout: App,
-    title: 'Edit Sending Domain'
+    title: 'Edit Sending Domain',
+    keywords: [
+      'sending domain'
+    ]
   },
   {
     path: '/account/smtp',
     component: SmtpPage,
     condition: hasGrants('api_keys/manage'),
     layout: App,
-    title: 'SMTP Settings'
+    title: 'SMTP Settings',
+    keywords: [
+      'smtp'
+    ]
   },
   {
     path: '/account/billing',
     component: SecretBillingPlanOrBillingSummaryPage,
     condition: all(hasGrants('account/manage'), not(isEnterprise), not(isHeroku), not(isAzure)),
     layout: App,
-    title: 'Billing'
+    title: 'Billing',
+    keywords: [
+      'billing'
+    ]
   },
   {
     path: '/account/billing/plan',
     component: billing.ChangePlanPage,
     condition: all(hasGrants('account/manage'), not(isEnterprise), not(isHeroku), not(isAzure)),
     layout: App,
-    title: 'Billing | Change My Plan'
+    title: 'Billing | Change My Plan',
+    keywords: [
+      'upgrade account'
+    ]
   },
   {
     path: '/account/ip-pools',
     component: ipPools.ListPage,
     condition: hasGrants('ip_pools/manage'),
     layout: App,
-    title: 'IP Pools'
+    title: 'IP Pools',
+    keywords: [
+      'ip pool'
+    ]
   },
   {
     path: '/account/ip-pools/create',
     component: ipPools.CreatePage,
     condition: hasGrants('ip_pools/manage'),
     layout: App,
-    title: 'New IP Pool'
+    title: 'New IP Pool',
+    keywords: [
+      'ip pool'
+    ]
   },
   {
     path: '/account/ip-pools/edit/:id',
     component: ipPools.EditPage,
     condition: hasGrants('ip_pools/manage'),
     layout: App,
-    title: 'Edit IP Pool'
+    title: 'Edit IP Pool',
+    keywords: [
+      'ip pool'
+    ]
   },
   {
     path: '/onboarding/plan',
     component: onboarding.ChoosePlan,
     condition: configFlag('featureFlags.has_signup'),
-    title: 'Onboarding | Choose Your Plan'
+    title: 'Onboarding | Choose Your Plan',
+    keywords: [
+      'upgrade account'
+    ]
   },
   {
     path: '/onboarding/sending-domain',
     component: onboarding.SendingDomainPage,
     condition: configFlag('featureFlags.has_signup'),
-    title: 'Onboarding | Create a Sending Domain'
+    title: 'Onboarding | Create a Sending Domain',
+    keywords: [
+      'sending domain'
+    ]
   },
   {
     path: '/onboarding/email',
     component: onboarding.SmtpOrApiPage,
     condition: configFlag('featureFlags.has_signup'),
-    title: 'Onboarding | REST and SMTP'
+    title: 'Onboarding | REST and SMTP',
+    keywords: [
+      'smtp'
+    ]
   },
   {
     path: '/onboarding/email/smtp',
     component: onboarding.SmtpPage,
     condition: configFlag('featureFlags.has_signup'),
-    title: 'Onboarding | Send a Test Email (SMTP)'
+    title: 'Onboarding | Send a Test Email (SMTP)',
+    keywords: [
+      'smtp'
+    ]
   },
   {
     path: '/onboarding/email/api',
     component: onboarding.ApiPage,
     condition: configFlag('featureFlags.has_signup'),
-    title: 'Onboarding | Send a Test Email (REST)'
+    title: 'Onboarding | Send a Test Email (REST)',
+    keywords: [
+      'smtp'
+    ]
   },
   {
     path: '/support/aws-premium',
     component: PremiumSupportPage,
     condition: isAws,
     title: 'Support | Request Premium Support',
-    layout: App
+    layout: App,
+    keywords: [
+      'upgrade plan'
+    ]
   },
   {
     path: '/logout',
