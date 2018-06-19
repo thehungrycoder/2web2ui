e34dac96a0a9d208a1b9910c6af8a89b27b40b60
Jason Rhodes <jason.rhodes@sparkpost.com>
2018-06-19 14:54:06 -0400

    FE-396 removed unused Loading from ChangePlanPage

--- src/pages/billing/ChangePlanPage.js
+++ src/pages/billing/ChangePlanPage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Page } from '@sparkpost/matchbox';
-import { Loading } from 'src/components';
 import { PendingPlanBanner } from './components/Banners';
 import ChangePlanForm from './forms/ChangePlanForm';
 import { canChangePlanSelector } from 'src/selectors/accountBillingInfo';
@@ -10,10 +9,6 @@ import { canChangePlanSelector } from 'src/selectors/accountBillingInfo';
 export class ChangePlanPage extends Component {
 
   render() {
-    if (this.props.loading) {
-      return <Loading />;
-    }
-
     return (
       <Page breadcrumbAction={{ content: 'Back to billing', to: '/account/billing', Component: Link }}>
         <PendingPlanBanner account={this.props.account} />
