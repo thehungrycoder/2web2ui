98f262e24303f0dae999aff07dc8cda4e518fd42
mhossain <mohammad.hossain@sparkpost.com>
2018-06-22 06:49:38 -0400

    remove unnecessary (not-so-sure) last arg

--- src/helpers/gtm/eventsMap.js
+++ src/helpers/gtm/eventsMap.js
@@ -9,9 +9,9 @@ function determineFormValidationState(action) {
 }
 const eventsMap = {
   '@@redux-form/INITIALIZE': getFormDefinition('Initialize'),
-  '@@redux-form/START_SUBMIT': getFormDefinition('Submit', null, 1),
-  '@@redux-form/SET_SUBMIT_SUCCEEDED': getFormDefinition('Submit Success', null, 1),
-  '@@redux-form/SET_SUBMIT_FAILED': getFormDefinition('Submit Failure', null, 0),
+  '@@redux-form/START_SUBMIT': getFormDefinition('Submit'),
+  '@@redux-form/SET_SUBMIT_SUCCEEDED': getFormDefinition('Submit Success'),
+  '@@redux-form/SET_SUBMIT_FAILED': getFormDefinition('Submit Failure'),
   '@@redux-form/UPDATE_SYNC_ERRORS': getFormDefinition(determineFormValidationState)
 };
 
