2864ab20b58d27a891acea3f95ae4b98a6ced361
mhossain <mohammad.hossain@sparkpost.com>
2018-06-29 12:51:51 -0400

    add condition mapping to reuse and to be DRY

--- src/config/supportIssues.js
+++ src/config/supportIssues.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import { hasOnlineSupport, hasStatus, hasStatusReasonCategory, isSuspendedForBilling } from 'src/helpers/conditions/account';
 import { all, not, any } from 'src/helpers/conditions';
 import { isAdmin } from 'src/helpers/conditions/user';
@@ -9,6 +11,14 @@ const COMPLIANCE = 'Compliance';
 const ERRORS = 'Errors';
 const LIMITS = 'DailyLimits';
 const SUPPORT = 'Support';
+
+const defaultCondition = all(hasOnlineSupport, hasStatus('active'));
+const idConditionsMap = {
+  general_billing: all(isAdmin, any(isSuspendedForBilling, hasStatus('active'))),
+  account_suspension: all(hasStatus('suspended'), not(hasStatusReasonCategory('100.01'))),
+  account_cancellation: isAdmin
+};
+
 
 /**
  * @example
@@ -24,8 +34,6 @@ const SUPPORT = 'Support';
  *
  *     type: 'Example',
  *
- *     // optional, composed account helpers to determine which user can report this issue
- *     condition: isEnterprise
  *   }
  */
 const supportIssues = [
@@ -33,100 +41,94 @@ const supportIssues = [
     id: 'ui_errors',
     label: 'UI errors',
     messageLabel: 'Tell us more about your issue',
-    type: ERRORS,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: ERRORS
   },
   {
     id: 'sending_domain_block',
     label: 'Sending domain block',
     messageLabel: 'Tell us more about your issue',
-    type: COMPLIANCE,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: COMPLIANCE
   },
   {
     id: 'configuration_setup_support',
     label: 'Configuration support',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   },
   {
     id: 'dns',
     label: 'DNS help',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   },
   {
     id: 'placement_deliverability',
     label: 'Deliverability issues',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   },
   {
     id: 'reporting_and_event_issue',
     label: 'Reporting & event issues',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   },
   {
     id: 'blacklisting',
     label: 'IP blacklisting',
     messageLabel: 'Tell us more about your issue',
-    type: COMPLIANCE,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: COMPLIANCE
   },
   {
     id: 'product/support_feedback',
     label: 'Feedback',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   },
   {
     id: 'account_upgrade/downgrade_issue',
     label: 'Account upgrade/downgrade issues',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   },
   {
     id: 'general_billing',
     label: 'Billing problems',
     messageLabel: 'Tell us more about your billing issue',
-    type: BILLING,
-    condition: all(isAdmin, any(isSuspendedForBilling, hasStatus('active')))
+    type: BILLING
   },
   {
     id: 'account_suspension',
     label: 'Account suspension',
     messageLabel: 'Why do you think your account should be unsuspended?',
-    type: COMPLIANCE,
-    condition: all(hasStatus('suspended'), not(hasStatusReasonCategory('100.01')))
+    type: COMPLIANCE
   },
   {
     id: 'daily_limits',
     label: 'Daily sending limit increase',
     messageLabel: 'What limit do you need and why?',
-    type: LIMITS,
-    condition: all(hasOnlineSupport, isAdmin, hasStatus('active'))
+    type: LIMITS
   },
   {
     id: 'account_cancellation',
     label: 'Account cancellation',
     messageLabel: 'Tell us why you are leaving',
-    type: COMPLIANCE,
-    condition: isAdmin
+    type: COMPLIANCE
   },
   {
     id: 'general_issue',
     label: 'Another issue',
     messageLabel: 'Tell us more about your issue',
-    type: SUPPORT,
-    condition: all(hasOnlineSupport, hasStatus('active'))
+    type: SUPPORT
   }
 ];
 
-export default supportIssues;
+
+const augmentIssuesWithConditions = function () {
+  return _.map(supportIssues, (supportIssue) => ({
+    ...supportIssue,
+    condition: idConditionsMap[supportIssue.id] || defaultCondition
+  }));
+};
+
+export default augmentIssuesWithConditions();
