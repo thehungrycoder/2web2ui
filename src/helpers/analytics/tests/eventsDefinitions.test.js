b0e347ec1bdc233f3c058e39605d8147f9e2c1d2
mhossain <mohammad.hossain@sparkpost.com>
2018-07-18 13:17:45 -0400

    Restore FE-368 (Revert "Revert "Merge branch 'feature/FE-368' into develop"")
    This reverts commit 1415e41206804371d9e9121e9d81bd35c915f092.

---
+++ src/helpers/analytics/tests/eventsDefinitions.test.js
@@ -0,0 +1,39 @@
+import cases from 'jest-in-case';
+import _ from 'lodash';
+
+import { getFormDefinition } from '../eventsDefinitions';
+
+describe('Event Definitions', () => {
+  let mockAction;
+  beforeEach(() => {
+    mockAction = {
+      type: null,
+      meta: {
+        form: 'formName'
+      },
+      payload: {}
+    };
+  });
+
+  const TEST_CASES = {
+    'Initialize': null,
+    'Submit': { value: 1 },
+    'Submit Failure': {},
+    'Submit Success': {},
+    'Validation Success': {}
+  };
+
+  cases('getFormDefinition', ({ name, value, ...rest }) => {
+    mockAction = _.assign(mockAction, rest);
+
+    expect(getFormDefinition(name, value)(mockAction)).toMatchSnapshot();
+  }, TEST_CASES);
+
+  describe('getFormDefinition', () => {
+    it('invokes the callback when provided', () => {
+      const fn = jest.fn().mockReturnValue({ action: 'Validation Error', label: 'formName: {"field1": "required"}' });
+      expect(getFormDefinition(fn)(mockAction)).toMatchSnapshot();
+      expect(fn).toHaveBeenCalledTimes(1);
+    });
+  });
+});
