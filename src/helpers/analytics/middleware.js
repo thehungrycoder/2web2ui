c5fd0cffcfacaa8571c2ad7974af31b7c4922283
Brian Kemper <brian.kemper@sparkpost.com>
2018-08-13 10:44:26 -0400

    FE-437 Renamed analytics setup file to middleware

--- src/helpers/analytics/middleware.js
+++ src/helpers/analytics/middleware.js
@@ -2,7 +2,6 @@ import { createMiddleware } from 'redux-beacon';
 import { pushEvents } from './index';
 import eventsMap from './eventsMap';
 
-
 const analyticsMiddleware = createMiddleware(eventsMap, pushEvents);
 
 export default analyticsMiddleware;
