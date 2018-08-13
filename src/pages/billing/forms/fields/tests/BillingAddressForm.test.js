ebc46089b2872b19365afeeebde601d5e62adc83
Jon Ambas <jon@jonambas.com>
2018-08-13 12:59:08 -0400

    FE-502 Fix billing country and state validation

--- src/pages/billing/forms/fields/tests/BillingAddressForm.test.js
+++ src/pages/billing/forms/fields/tests/BillingAddressForm.test.js
@@ -9,13 +9,16 @@ describe('Billing Address Form:', () => {
     formName: 'form-name',
     countryValue: 'GG',
     countries: [
-      { value: 'US', label: 'USOFA', states: [{ name: 'mrylnd', value: 'MD' }]}
+      { value: 'US', label: 'USOFA', states: [{ name: 'mrylnd', value: 'MD' }]},
+      { value: 'CA', label: 'CANADA', states: [{ name: 'alberta', value: 'AB' }]}
     ],
-    change: jest.fn()
+    change: jest.fn(),
+    firstState: 'FirstState'
   };
 
   beforeEach(() => {
     wrapper = shallow(<BillingAddressForm {...props} />);
+    jest.clearAllMocks();
   });
 
   it('should render', () => {
@@ -39,12 +42,25 @@ describe('Billing Address Form:', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should handle state value', () => {
-    const changeSpy = jest.spyOn(wrapper.instance().props, 'change');
-    wrapper.instance().handleCountryChange({ target: { value: 'US' }});
-    expect(changeSpy).not.toHaveBeenCalled();
-    changeSpy.mockReset();
-    wrapper.instance().handleCountryChange({ target: { value: 'GG' }});
-    expect(changeSpy).toHaveBeenCalledWith(props.formName, 'billingAddress.state', null);
+  describe('on component did update', () => {
+    it('should select first state if US is selected', () => {
+      wrapper.setProps({ countryValue: 'US' });
+      expect(props.change).toHaveBeenCalledWith(props.formName, 'billingAddress.state', props.firstState);
+    });
+
+    it('should select first state if CA is selected', () => {
+      wrapper.setProps({ countryValue: 'CA' });
+      expect(props.change).toHaveBeenCalledWith(props.formName, 'billingAddress.state', props.firstState);
+    });
+
+    it('should deselect state if not US or Canada', () => {
+      wrapper.setProps({ countryValue: 'AF' });
+      expect(props.change).toHaveBeenCalledWith(props.formName, 'billingAddress.state', null);
+    });
+
+    it('should not do anything if country has not changed', () => {
+      wrapper.setProps({ firstState: 'MD' });
+      expect(props.change).not.toHaveBeenCalled();
+    });
   });
 });
