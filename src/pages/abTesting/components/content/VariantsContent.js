5a4ab35de3f2a7756e1839156fb5a2a4f79a7a06
Andrew Winder <andrew.winder@sparkpost.com>
2018-08-22 16:10:59 -0400

    EO-211 Updating to use pagelink so that the page doesnt hard refresh

--- src/pages/abTesting/components/content/VariantsContent.js
+++ src/pages/abTesting/components/content/VariantsContent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { UnstyledLink } from '@sparkpost/matchbox';
+import { PageLink } from 'src/components';
 
 const VariantsContent = () => (
   <div>
     <p>The templates you've selected will appear here, along with their results once this test concludes.</p>
-    <p>If you need to create a new template, <UnstyledLink to='/templates'>head over to the templates page</UnstyledLink>.</p>
+    <p>If you need to create a new template, <PageLink to='/templates'>head over to the templates page</PageLink>.</p>
   </div>
 );
 
