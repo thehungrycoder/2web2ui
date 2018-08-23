c1af3a3342043eab9ae9581eefe18bc44659fa8b
Jon Ambas <jon@jonambas.com>
2018-08-23 15:31:33 -0400

    FE-478 Rework getting started checklist

--- src/pages/dashboard/components/Tutorial.js
+++ src/pages/dashboard/components/Tutorial.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { Panel } from '@sparkpost/matchbox';
+import { OpenInNew } from '@sparkpost/matchbox-icons';
 import TutorialItem from './TutorialItem';
 
 const sendingDomainUrl = 'account/sending-domains';
@@ -11,63 +13,51 @@ export class Tutorial extends Component {
       hasSendingDomains,
       hasVerifiedDomains,
       hasApiKeysForSending,
-      hasBounceDomains,
       hasSentThisMonth
     } = this.props;
 
     return (
-      <Panel title='Getting Started Checklist'>
-        <Panel.Section>
-          <TutorialItem
-            label='Verify your email address'
-            completed={currentUser.email_verified} >
-            <p>Check your email to verify your email address and increase your daily sending limits</p>
-          </TutorialItem>
-        </Panel.Section>
+      <Panel title='Getting Started Checklist' actions={[{
+        content: <Fragment>Need help? Check out our Getting Started guide <OpenInNew size={13}/></Fragment>,
+        color: 'orange',
+        external: true,
+        to: 'https://www.sparkpost.com/docs/getting-started/getting-started-sparkpost/' }]}>
 
-        <Panel.Section>
-          <TutorialItem
-            label='Add a sending domain'
-            labelLink={sendingDomainUrl}
-            completed={hasSendingDomains}>
-            <p>Add a sending domain to send email from your own domain</p>
-          </TutorialItem>
-        </Panel.Section>
+        <TutorialItem
+          label='Verify your email address'
+          completed={currentUser.email_verified}>
+          <p>So we know you're legitimate, check your email to verify your email address. This also unlocks higher daily sending limits.</p>
+        </TutorialItem>
 
-        <Panel.Section>
-          <TutorialItem
-            label='Verify your sending domain'
-            labelLink={sendingDomainUrl}
-            completed={hasVerifiedDomains}>
-            <p>You'll need to verify your domain before you can use it</p>
-          </TutorialItem>
-        </Panel.Section>
+        <TutorialItem
+          label='Add a sending domain'
+          labelLink={sendingDomainUrl}
+          actions={[{ content: 'Add Sending Domain', color: 'orange', component: Link, to: '/account/sending-domains/create' }]}
+          completed={hasSendingDomains}>
+          <p>Adding a sending domain will allow you to send email from your own domain.</p>
+        </TutorialItem>
 
-        <Panel.Section>
-          <TutorialItem
-            label='Create bounce domain'
-            labelLink={sendingDomainUrl}
-            completed={hasBounceDomains}>
-            <p>Create a bounce domain to track email engagement</p>
-          </TutorialItem>
-        </Panel.Section>
+        <TutorialItem
+          label='Verify your sending domain'
+          labelLink={sendingDomainUrl}
+          actions={[{ content: 'View Sending Domains', color: 'orange', component: Link, to: '/account/sending-domains' }]}
+          completed={hasVerifiedDomains}>
+          <p>You'll need to verify your domain before you can use it.</p>
+        </TutorialItem>
 
-        <Panel.Section>
-          <TutorialItem
-            label='Create an API key'
-            labelLink='account/api-keys'
-            completed={hasApiKeysForSending}>
-            <p>You'll need to create an API key to use our API or SMTP integration.</p>
-          </TutorialItem>
-        </Panel.Section>
+        <TutorialItem
+          label='Get an API key for sending email'
+          labelLink='account/api-keys'
+          actions={[{ content: 'Create an API Key', color: 'orange', component: Link, to: '/account/api-keys/create' }]}
+          completed={hasApiKeysForSending}>
+          <p>You'll need to create an API key with the correct permissions to send email.</p>
+        </TutorialItem>
 
-        <Panel.Section>
-          <TutorialItem
-            label='Send an email this month'
-            completed={hasSentThisMonth}>
-            <p>Get sending!</p>
-          </TutorialItem>
-        </Panel.Section>
+        <TutorialItem
+          label='Send an email'
+          completed={hasSentThisMonth}>
+          <p>Get sending!</p>
+        </TutorialItem>
       </Panel>
     );
   }
--- src/pages/dashboard/components/TutorialItem.js
+++ src/pages/dashboard/components/TutorialItem.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import classnames from 'classnames';
-import { Check, ArrowForward } from '@sparkpost/matchbox-icons';
+import { Panel } from '@sparkpost/matchbox';
+import { Check } from '@sparkpost/matchbox-icons';
 import styles from './TutorialItem.module.scss';
 
 const TutorialItem = ({
   label,
   children,
   labelLink,
-  completed
+  completed,
+  actions
 }) => {
   const itemClasses = classnames(styles.Item, completed && styles.completed);
-
-  const labelMarkup = (labelLink && !completed)
-    ? <h6 className={classnames(styles.Label, styles.link)}><Link to={labelLink}>{ label } <ArrowForward /></Link></h6>
-    : <h6 className={styles.Label}>{ label }</h6>;
+  const labelMarkup = <h6 className={styles.Label}>{label}</h6>;
 
   const completedMarkup = completed
-    ? <Check size={24} className={styles.Completed} />
-    : null;
+    ? (
+      <div className={styles.CompletedBackDrop}>
+        <Check size={21} className={styles.Completed} />
+      </div>
+    )
+    : <div className={styles.NotCompleted} />;
 
   return (
-    <div className={itemClasses}>
-      { completedMarkup }
-      <div className={styles.Content}>
-        { labelMarkup }
-        {!completed && <div className={styles.HelpText}>{ children }</div>}
+    <Panel.Section actions={actions}>
+      <div className={itemClasses}>
+        <div className={styles.Icon}>
+          {completedMarkup}
+        </div>
+        <div className={styles.Content}>
+          {labelMarkup}
+          <div className={styles.HelpText}>{children}</div>
+        </div>
       </div>
-    </div>
+    </Panel.Section>
   );
 };
 
