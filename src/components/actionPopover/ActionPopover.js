3f6669294894a9104c4b59f445119b7b4aa69fbc
Daniel Milburn <daniel.s.milburn@gmail.com>
2018-09-06 17:04:36 -0400

    FE-528 WIP on edit user page

---
+++ src/components/actionPopover/ActionPopover.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Popover, Button, ActionList } from '@sparkpost/matchbox';
+import { MoreHoriz } from '@sparkpost/matchbox-icons';
+
+const ActionPopover = ({ actions }) => <Popover left trigger={<Button flat><MoreHoriz size={20}/></Button>}>
+  <ActionList actions={actions}/>
+</Popover>;
+
+export default ActionPopover;
