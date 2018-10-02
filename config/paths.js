a6215c87cc1cae38b9ec7cf4c364fcdb72a81550
mhossain <mohammad.hossain@sparkpost.com>
2018-10-02 16:12:17 -0400

    copy scripts

--- config/paths.js
+++ config/paths.js
@@ -54,6 +54,7 @@ module.exports = {
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
   copyPaths: [
+    [`${appDirectory}/ci/scripts`, 'scripts'],
     [`${appDirectory}/ci/appspec.yml`, 'appspec.yml']
   ]
 };
