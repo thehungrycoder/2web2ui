7cfee9c0d870e36ea507fed8ec9d68ea6b366ffc
Brian Kemper <brian.kemper@sparkpost.com>
2018-11-02 15:23:26 -0400

    TR-600 Updated ActionsTadleData snapshots

--- src/pages/snippets/components/ActionsTableData.js
+++ src/pages/snippets/components/ActionsTableData.js
@@ -3,25 +3,25 @@ import ActionPopover from 'src/components/actionPopover';
 import PageLink from 'src/components/pageLink';
 import { setSubaccountQuery } from 'src/helpers/subaccounts';
 
-const ActionsTableData = ({ id, subaccount_id, toggleDelete }) => (
+const ActionsTableData = ({ id, subaccount_id: subaccountId, toggleDelete }) => (
   <ActionPopover
     actions={[
       {
         Component: PageLink,
         content: 'Edit',
-        to: `/snippets/edit/${id}${setSubaccountQuery(subaccount_id)}`
+        to: `/snippets/edit/${id}${setSubaccountQuery(subaccountId)}`
       },
       {
         Component: PageLink,
         content: 'Duplicate',
         to: {
           pathname: '/snippets/create',
-          state: { id, subaccount_id }
+          state: { id, subaccountId }
         }
       },
       {
         content: 'Delete',
-        onClick: () => toggleDelete(id, subaccount_id)
+        onClick: () => toggleDelete(id, subaccountId)
       }
     ]}
   />
