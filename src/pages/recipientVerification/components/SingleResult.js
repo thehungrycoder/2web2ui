3473222f73c6e6b0589ae93f608d6e77ccfba013
21042jim <21042jim@gmail.com>
2018-11-03 00:22:30 -0400

    css/PR comments

--- src/pages/recipientVerification/components/SingleResult.js
+++ src/pages/recipientVerification/components/SingleResult.js
@@ -1,40 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { CheckCircleOutline, Warning } from '@sparkpost/matchbox-icons';
 import styles from './SingleAddressForm.module.scss';
 
 const validText = ' is a valid email address.';
 const invalidText = ' is NOT a valid email address.';
 
-export class SingleResult extends Component {
+const SingleResult = ({ singleResults }) => {
+  const { valid, reason, email } = singleResults;
+  const text = valid ? validText : `${invalidText} ${reason}`;
+  const icon = valid
+    ? <CheckCircleOutline className={styles.validIcon} size={33}/>
+    : <Warning className={styles.invalidIcon} size={33} />;
 
-  renderValid() {
-    const { email } = this.props.singleResults;
-
-    return <p>
-      <CheckCircleOutline className={styles.validIcon}/>
-      <span className={styles.email}> {email}</span>
-      <span className={styles.Paragraph}>{validText}</span>
-    </p>;
-  }
-
-  renderInvalid() {
-    const { email, reason } = this.props.singleResults;
-
-    return <p>
-      <Warning className={styles.invalidIcon}/>
-      <span className={styles.email}> {email}</span>
-      <span className={styles.Paragraph}>{invalidText} {reason}.</span>
-    </p>;
-  }
-
-  render() {
-    const { valid } = this.props.singleResults;
-
-    return <Fragment>
+  return (
+    <Fragment>
       <br/>
-      {(valid) ? this.renderValid() : this.renderInvalid()}
-    </Fragment>;
-  }
-}
+      <p>
+        <span className={styles.IconWrapper}>
+          {icon}
+        </span>
+        <strong className={styles.email}> {email}</strong>
+        <span className={styles.Paragraph}>{text}.</span>
+      </p>
+    </Fragment>
+  );
+};
 
 export default SingleResult;
