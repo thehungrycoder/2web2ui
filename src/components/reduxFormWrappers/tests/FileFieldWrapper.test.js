1043f289181d99b6454485c464c095b652417bcd
Jon Ambas <jon@jonambas.com>
2018-11-05 11:31:35 -0500

    EO-331 Add label hidden test

--- src/components/reduxFormWrappers/tests/FileFieldWrapper.test.js
+++ src/components/reduxFormWrappers/tests/FileFieldWrapper.test.js
@@ -41,6 +41,11 @@ cases('FileFieldWrapper', (props) => {
   },
   'renders file input with help message': {
     helpText: 'Ask Google!'
+  },
+  'renders with a hidden label': {
+    labelHidden: true,
+    required: true,
+    label: 'a label'
   }
 });
 
