2db9dbe5acb8604ed448439ad7981508d669686e
Jon Ambas <jon@jonambas.com>
2018-12-10 18:02:05 -0500

    EO-435 Add tests

--- src/pages/signals/OverviewPage.js
+++ src/pages/signals/OverviewPage.js
@@ -14,7 +14,6 @@ export class OverviewPage extends Component {
   }
 
   handleClick = ({ payload }) => {
-    console.log(payload);
     this.setState({ selected: payload.key });
   }
 
@@ -32,17 +31,20 @@ export class OverviewPage extends Component {
         <SummaryTable title='Engagement Cohort' />
 
         <Panel sectioned>
-          <DivergingBar data={[
-            { key: 'abc', label: 'Label 1', value: 0.8 },
-            { key: 'def', label: 'Label 2', value: -0.5 },
-            { key: 'ghi', label: 'Label 3', value: -0.1 },
-            { key: 'jkl', label: 'Label 4', value: 0.3 },
-            { key: 'mno', label: 'Label 5', value: 0.5 },
-            { key: 'pqr', label: 'Label 6', value: -0.15 }
-          ]}
-          selected={this.state.selected}
-          onClick={this.handleClick}
-          />
+          <div style={{ width: '350px' }}>
+            <DivergingBar data={[
+              { key: 'abc', label: 'Label 1', value: 0.8 },
+              { key: 'def', label: 'Label 2', value: -0.5 },
+              { key: 'xyz', label: 'Label 7', value: -0.22 },
+              { key: 'ghi', label: 'Label 3', value: -0.1 },
+              { key: 'jkl', label: 'Label 4', value: 0.3 },
+              { key: 'mno', label: 'Label 5', value: 0.5 },
+              { key: 'pqr', label: 'Label 6', value: -0.15 }
+            ]}
+            selected={this.state.selected}
+            onClick={this.handleClick}
+            height={280}
+            /></div>
         </Panel>
       </Page>
     );
--- src/pages/signals/components/charts/divergingBar/DivergingBar.js
+++ src/pages/signals/components/charts/divergingBar/DivergingBar.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ResponsiveContainer, BarChart, Bar, Tooltip, XAxis, YAxis, CartesianGrid, Rectangle } from 'recharts';
+import { ResponsiveContainer, BarChart, Bar, Tooltip, XAxis, YAxis, CartesianGrid, Rectangle, Text } from 'recharts';
 import TooltipWrapper from '../tooltip/Tooltip';
 import _ from 'lodash';
 import './DivergingBar.scss';
 
-class DivergingBar extends React.Component {
+class DivergingBar extends Component {
   getData = () => {
     const { data, xKey, positiveFill, negativeFill } = this.props;
     return _.sortBy(data, (item) => item[xKey]).map((item) => ({ ...item, fill: item[xKey] > 0 ? positiveFill : negativeFill }));
@@ -31,15 +31,49 @@ class DivergingBar extends React.Component {
     );
   }
 
+  renderYTick = ({ payload, ...props }) => {
+    const { data, selected, yKey } = this.props;
+    const match = _.find(data, ['key', selected]) || {};
+
+    if (payload.value === match[yKey]) {
+      return <Text {...props} fill='#0B83D6'>{payload.value}</Text>;
+    }
+
+    return <Text {...props}>{payload.value}</Text>;
+  }
+
   render() {
     const { height, tooltipContent, onClick, width, xDomain, xKey, yKey } = this.props;
     return (
       <ResponsiveContainer height={height} width={width} className='DivergingBar'>
-        <BarChart data={this.getData()} layout='vertical' barCategoryGap={1}>
-          <CartesianGrid horizontal={false} shapeRendering='crispEdges' />
-          <Bar cursor='pointer' dataKey={xKey} onClick={onClick} isAnimationActive={false} shape={this.renderBar} />
-          <YAxis type='category' tickLine={false} axisLine={false} interval={0} dataKey={yKey} />
-          <XAxis type='number' tickLine={false} domain={xDomain} dataKey={xKey} shapeRendering='crispEdges'/>
+        <BarChart data={this.getData()} layout='vertical' barCategoryGap={2}>
+          <CartesianGrid
+            horizontal={false}
+            shapeRendering='crispEdges'
+          />
+          <Bar
+            cursor='pointer'
+            dataKey={xKey}
+            onClick={onClick}
+            isAnimationActive={false}
+            shape={this.renderBar}
+          />
+          <YAxis
+            type='category'
+            tickLine={false}
+            axisLine={false}
+            interval={0}
+            dataKey={yKey}
+            padding={{ bottom: 5 }}
+            tick={this.renderYTick}
+          />
+          <XAxis
+            type='number'
+            tickLine={false}
+            domain={xDomain}
+            dataKey={xKey}
+            shapeRendering='crispEdges'
+          />
           <Tooltip
             cursor={false}
             isAnimationActive={false}
@@ -52,14 +86,23 @@ class DivergingBar extends React.Component {
 }
 
 DivergingBar.propTypes = {
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  negativeFill: PropTypes.string,
+  onClick: PropTypes.func,
+  positiveFill: PropTypes.string,
+  tooltipContent: PropTypes.func,
+  xDomain: PropTypes.array,
+  xKey: PropTypes.string,
+  yKey: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 };
 
 DivergingBar.defaultProps = {
+  height: 250,
   xDomain: ['auto', 'auto'],
   xKey: 'value',
   yKey: 'label',
-  height: 250,
-  width: 400,
+  width: '99%',
   negativeFill: '#DB2F2D',
   positiveFill: '#8CBE3C'
 };
