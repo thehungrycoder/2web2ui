e099fbd57e5e658df7f650fb9a6ac6158f71857e
Brian Kemper <brian.kemper@sparkpost.com>
2018-12-11 14:47:17 -0500

    EO-426 Signals facet filter

---
+++ src/config/facets.js
@@ -0,0 +1,16 @@
+const facets = [
+  {
+    key: 'campaign',
+    label: 'Campaign'
+  },
+  {
+    key: 'ip_pool',
+    label: 'IP Pool'
+  },
+  {
+    key: 'sending_domain',
+    label: 'Sending Domain'
+  }
+];
+
+export default facets;
--- src/pages/signals/components/filters/FacetFilter.js
+++ src/pages/signals/components/filters/FacetFilter.js
@@ -1,18 +1,68 @@
 import React from 'react';
-import { Select } from '@sparkpost/matchbox';
+import { Select, TextField } from '@sparkpost/matchbox';
+import { Search } from '@sparkpost/matchbox-icons';
+import facets from 'src/config/facets';
+import { onEnter } from 'src/helpers/keyEvents';
+import withSignalOptions from '../withSignalOptions';
 import styles from './FacetFilter.module.scss';
 
 const OPTIONS = [
-  'No Breakdown',
-  'By Sending Domain',
-  'By IP Pool',
-  'By Campaign'
+  { label: 'No Breakdown', value: '' },
+  ...facets.map(({ key, label }) => ({ label: `By ${label}`, value: key }))
 ];
 
-const FacetFilter = (props) => (
-  <div className={styles.FacetFilter}>
-    <Select options={OPTIONS} />
-  </div>
-);
+export class FacetFilter extends React.Component {
+  state = {
+    searchTerm: ''
+  }
 
-export default FacetFilter;
+  static getDerivedStateFromProps(nextProps, nextState) {
+    return {
+      searchTerm: nextProps.facetSearchTerm
+    };
+  }
+
+  handleFacetChange = (event) => {
+    const { changeSignalOptions } = this.props;
+    changeSignalOptions({ facet: event.currentTarget.value, facetSearchTerm: '' });
+  }
+
+  handleFacetSearch = () => {
+    const { changeSignalOptions } = this.props;
+    changeSignalOptions({ facetSearchTerm: this.state.searchTerm });
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({ searchTerm: event.currentTarget.value });
+  }
+
+  render() {
+    const { facet } = this.props;
+    const { searchTerm } = this.state;
+
+    return (
+      <React.Fragment>
+        <div className={styles.FacetFilter}>
+          <Select
+            onChange={this.handleFacetChange}
+            options={OPTIONS}
+            value={facet}
+          />
+        </div>
+        {facet && (
+          <div className={styles.FacetFilter}>
+            <TextField
+              onChange={this.handleSearchChange}
+              onKeyPress={onEnter(this.handleFacetSearch)}
+              placeholder="Search here"
+              suffix={<Search />}
+              value={searchTerm}
+            />
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default withSignalOptions(FacetFilter);
