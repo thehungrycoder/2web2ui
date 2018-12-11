fc3ae0c8146a312d0e0ed602f7759363a03cf1c3
Brian Kemper <brian.kemper@sparkpost.com>
2018-12-11 10:51:59 -0500

    EO-426 Signals date filter

--- src/pages/signals/components/filters/tests/DateFilter.test.js
+++ src/pages/signals/components/filters/tests/DateFilter.test.js
@@ -1,16 +1,20 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import DateFilter from '../DateFilter';
+import { DateFilter } from '../DateFilter';
 
-describe('DateFilter Component', () => {
-  let wrapper;
+describe('DateFilter', () => {
+  const subject = (props = {}) => shallow(<DateFilter {...props} />);
 
-  beforeEach(() => {
-    const props = {};
-    wrapper = shallow(<DateFilter {...props}/>);
+  it('renders a select', () => {
+    expect(subject()).toMatchSnapshot();
   });
 
-  it('renders correctly', () => {
-    expect(wrapper).toMatchSnapshot();
+  it('calls changeSignalOptions when select changes', () => {
+    const changeSignalOptions = jest.fn();
+    const wrapper = subject({ changeSignalOptions });
+
+    wrapper.find('Select').simulate('change', { currentTarget: { value: '14days' }});
+
+    expect(changeSignalOptions).toHaveBeenCalledWith({ relativeRange: '14days' });
   });
 });
