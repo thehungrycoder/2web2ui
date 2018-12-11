ecd3a9c7845ac3819b25abc9bc5cf5ae299b24fb
Jon Ambas <jon@jonambas.com>
2018-12-11 11:56:43 -0500

    EO-435 Remove demo code

--- src/pages/signals/OverviewPage.js
+++ src/pages/signals/OverviewPage.js
@@ -5,18 +5,7 @@ import FacetFilter from './components/filters/FacetFilter';
 import DateFilter from './components/filters/DateFilter';
 import SubaccountFilter from './components/filters/SubaccountFilter';
 
-import { Panel } from '@sparkpost/matchbox';
-import DivergingBar from './components/charts/divergingBar/DivergingBar';
-
 export class OverviewPage extends Component {
-  state = {
-    selected: ''
-  }
-
-  handleClick = ({ payload }) => {
-    this.setState({ selected: payload.key });
-  }
-
   render() {
     return (
       <Page primaryArea={
@@ -29,23 +18,6 @@ export class OverviewPage extends Component {
         <SummaryTable title='Health Score' />
         <SummaryTable title='Spam Traps' />
         <SummaryTable title='Engagement Cohort' />
-
-        <Panel sectioned>
-          <div style={{ width: '350px' }}>
-            <DivergingBar data={[
-              { key: 'abc', label: 'Label 1', value: 0.8 },
-              { key: 'def', label: 'Label 2', value: -0.5 },
-              { key: 'xyz', label: 'Label 7', value: -0.22 },
-              { key: 'ghi', label: 'Label 3', value: -0.1 },
-              { key: 'jkl', label: 'Label 4', value: 0.3 },
-              { key: 'mno', label: 'Label 5', value: 0.5 },
-              { key: 'pqr', label: 'Label 6', value: -0.15 }
-            ]}
-            selected={this.state.selected}
-            onClick={this.handleClick}
-            height={280}
-            /></div>
-        </Panel>
       </Page>
     );
   }
