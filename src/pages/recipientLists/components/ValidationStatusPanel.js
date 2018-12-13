e82fce5890734e326de31fc123a23b25c742195a
Patrick Sison <patrick.m.sison@gmail.com>
2018-12-13 10:20:31 -0500

    hotfix

--- src/pages/recipientLists/components/ValidationStatusPanel.js
+++ src/pages/recipientLists/components/ValidationStatusPanel.js
@@ -25,20 +25,24 @@ class ValidationStatusPanel extends React.Component {
     complete: false
   }
 
-  // componentDidMount() {
-  //   this.props.getLatestJob();
-  //   const { results, stopPolling, latestId, startPolling } = this.props;
-  //   stopPolling(latestId);
-  //   if (!results.complete) {
-  //     this.handlePoll(latestId);
-  //     startPolling({
-  //       key: latestId,
-  //       action: () => this.handlePoll(latestId),
-  //       interval: 5000
-  //     });
-  //   }
-  // }
+  componentDidMount() {
+    this.props.getLatestJob();
+  }
 
+  componentDidUpdate() {
+    const { results, stopPolling, latestId, startPolling } = this.props;
+    if (latestId) {
+      stopPolling(latestId);
+      if (!results.complete) {
+        this.handlePoll(latestId);
+        startPolling({
+          key: latestId,
+          action: () => this.handlePoll(latestId),
+          interval: 5000
+        });
+      }
+    }
+  }
 
   onCheckboxChange = (e) => {
     this.setState({ checkbox: !this.state.checkbox });
@@ -48,6 +52,7 @@ class ValidationStatusPanel extends React.Component {
     const { showAlert, getJobStatus, stopPolling } = this.props;
     return getJobStatus(id).then(({ complete }) => {
       if (complete) {
+        this.setState({ complete: true });
         stopPolling(id);
         showAlert({
           type: 'success',
