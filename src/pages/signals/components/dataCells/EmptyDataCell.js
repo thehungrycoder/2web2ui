3db20bc08bf1a2a741ae0f026cdcf1b424759dc0
Brian Kemper <brian.kemper@sparkpost.com>
2019-01-02 13:45:49 -0500

    EO-430 EmptyDataCell component

---
+++ src/pages/signals/components/dataCells/EmptyDataCell.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './DataCell.module.scss';
+
+const EmptyDataCell = () => (
+  <div className={styles.Center}>
+    No Data
+  </div>
+);
+
+export default EmptyDataCell;
