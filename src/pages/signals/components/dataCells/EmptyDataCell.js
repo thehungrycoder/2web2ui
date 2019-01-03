e60ac61cd9a602a42446e8da5956997c192593ed
Brian Kemper <brian.kemper@sparkpost.com>
2019-01-03 06:41:57 -0500

    EO-430 Italicized EmptyDataCell text

--- src/pages/signals/components/dataCells/EmptyDataCell.js
+++ src/pages/signals/components/dataCells/EmptyDataCell.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './DataCell.module.scss';
 
 const EmptyDataCell = () => (
-  <div className={styles.Center}>
+  <div className={styles.EmptyDataCell}>
     No Data
   </div>
 );
