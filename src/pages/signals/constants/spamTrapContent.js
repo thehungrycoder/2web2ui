770bbd49ce7eaba3599aabe5374c0032afbf78f2
Jon Ambas <jon@jonambas.com>
2019-01-22 11:21:45 -0500

    EO-502 Add line break after first sentence of each action

--- src/pages/signals/constants/spamTrapContent.js
+++ src/pages/signals/constants/spamTrapContent.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const content = [
   {
     condition: (n) => n < 0.0005,
     content: (
-      <span>
-        <strong>You are sending email to few or no spam traps.</strong> Good job with your list hygiene practices!
-      </span>
+      <Fragment>
+        <strong>You are sending email to few or no spam traps.</strong><br/>
+        Good job with your list hygiene practices!
+      </Fragment>
     ),
     type: 'good'
   },
   {
     condition: (n) => n >= 0.0005 && n < 0.001,
     content: (
-      <span>
-        <strong>You are hitting some spam traps that could lead to future deliverability issues.</strong> There are ways to accomplish a healthy list of engaged subscribers. It's key that you keep your subscribers engaged and that you remove unengaged recipients and replace them with new subscribers who are interested in your email.
-      </span>
+      <Fragment>
+        <strong>You are hitting some spam traps that could lead to future deliverability issues.</strong><br/>
+        There are ways to accomplish a healthy list of engaged subscribers. It's key that you keep your subscribers engaged and that you remove unengaged recipients and replace them with new subscribers who are interested in your email.
+      </Fragment>
     ),
     type: 'warning',
     link: 'https://www.sparkpost.com/docs/signals/engagement/'
@@ -23,9 +25,10 @@ const content = [
   {
     condition: (n) => n >= 0.001 && n < 0.01,
     content: (
-      <span>
-        <strong>You are sending email to a large number of known spam traps.</strong> Hitting spam traps is an indicator of poor list procurement and hygiene practices and has a strong negative impact on your overall email deliverability. You should take steps to improve your list hygiene.
-      </span>
+      <Fragment>
+        <strong>You are sending email to a large number of known spam traps.</strong><br/>
+        Hitting spam traps is an indicator of poor list procurement and hygiene practices and has a strong negative impact on your overall email deliverability. You should take steps to improve your list hygiene.
+      </Fragment>
     ),
     type: 'bad',
     link: 'https://www.sparkpost.com/docs/signals/list-hygiene/'
@@ -33,9 +36,10 @@ const content = [
   {
     condition: (n) => n >= 0.01,
     content: (
-      <span>
-        <strong>You are sending email to a very large number of known spam traps.</strong> Hitting spam traps is an indicator of poor list procurement and hygiene practices and has a strong negative impact on your overall email deliverability. You should take steps to improve your list hygiene immediately.
-      </span>
+      <Fragment>
+        <strong>You are sending email to a very large number of known spam traps.</strong><br/>
+        Hitting spam traps is an indicator of poor list procurement and hygiene practices and has a strong negative impact on your overall email deliverability. You should take steps to improve your list hygiene immediately.
+      </Fragment>
     ),
     type: 'bad',
     link: 'https://www.sparkpost.com/docs/signals/subscriber-acquisition/'
