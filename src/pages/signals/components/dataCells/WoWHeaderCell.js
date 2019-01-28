83bc7822325ce83f99149f2673a996a7d0c23664
Jon Ambas <jon@jonambas.com>
2019-01-28 15:30:21 -0500

    EO-547 Update wow tooltip copy

--- src/pages/signals/components/dataCells/WoWHeaderCell.js
+++ src/pages/signals/components/dataCells/WoWHeaderCell.js
@@ -4,7 +4,7 @@ import { InfoOutline } from '@sparkpost/matchbox-icons';
 import styles from './DataCell.module.scss';
 
 const WoWHeaderCell = () => (
-  <Tooltip content='Weekly Change' dark>
+  <Tooltip content='Week Over Week Change' dark>
     WoW <InfoOutline size={15} className={styles.InfoIcon}/>
   </Tooltip>
 );
