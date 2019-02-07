b7c095b5ad74ef81746dae3c885798cb8e8e3469
Jon Ambas <jon@jonambas.com>
2019-02-07 11:48:46 -0500

    EO-575 Correctly position NEW cohort

--- src/pages/signals/constants/cohorts.js
+++ src/pages/signals/constants/cohorts.js
@@ -1,27 +1,27 @@
 export default {
-  c_uneng: {
+  c_new: {
     fill: '#E4EEF0',
+    label: 'New',
+    description: 'Never engaged, first email in last 7 days'
+  },
+  c_uneng: {
+    fill: '#B9E2E9',
     label: 'Never Engaged',
     description: '365+ days since last engagement'
   },
   c_365d: {
-    fill: '#B9E2E9',
+    fill: '#9AD8E3',
     label: 'Not Recently Engaged',
-    description: '90 to 365 days since last engagement'
+    description: '90-365 days since last engagement'
   },
   c_90d: {
-    fill: '#9AD8E3',
+    fill: '#44A8AD',
     label: 'Semi Recently Engaged',
-    description: '14 to 90 days since last engagement'
+    description: '14-90 days since last engagement'
   },
   c_14d: {
-    fill: '#44A8AD',
-    label: 'Recently Engaged',
-    description: '0 to 14 days since last engagement'
-  },
-  c_new: {
     fill: '#3F8B93',
-    label: 'New',
-    description: 'Never engaged, first email in last 7 days'
+    label: 'Recently Engaged',
+    description: '0-14 days since last engagement'
   }
 };
